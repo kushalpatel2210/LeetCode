e56f0527a2ecdf812e9fcb23259e00de83a49067
Kushal Patel <61028285+kushalpatel2210@users.noreply.github.com>
2024-12-13 17:54:44 -0500

    Time: 44 ms (52.88%), Space: 19.4 MB (33.5%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.py
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.py
@@ -6,6 +6,28 @@
 
 class Solution:
     def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        slow, fast = head, head
+        result = None
+
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+            if slow == fast:
+                break
+        else:
+            return None
+        
+        entry = head
+
+        while entry != slow:
+            entry = entry.next
+            slow = slow.next
+        return entry
+        
+'''
+class Solution:
+    def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
         seen = set()
 
         while head and head.next:
@@ -15,3 +37,4 @@ class Solution:
             head = head.next
         
         return None
+'''
