9f6e09aa25574270bc7a759814b2487ed67f33d4
Kushal Patel <61028285+kushalpatel2210@users.noreply.github.com>
2024-12-13 16:52:05 -0500

    Time: 46 ms (51.68%), Space: 19.3 MB (30.76%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.py
+++ 0141-linked-list-cycle/0141-linked-list-cycle.py
@@ -4,6 +4,22 @@
 #         self.val = x
 #         self.next = None
 
+# Using Fast and Slow pointer
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        slow, fast = head, head
+
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+            if slow == fast:
+                return True
+        
+        return False
+
+'''
+# Using set 
 class Solution:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
         nodes = set()
@@ -16,3 +32,4 @@ class Solution:
                 return True
         
         return False
+'''
