2ad0480bf6ade44c2eb43f095a9803f56934eff6
Kushal Patel <61028285+kushalpatel2210@users.noreply.github.com>
2025-01-26 12:17:35 -0500

    Time: 3 ms (92.02%), Space: 21.3 MB (36.19%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,28 +1,12 @@
 from collections import Counter
-import heapq
 
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        count = Counter(nums)
-        most_common = count.most_common(k)
-        result = [element[0] for element in most_common]
-
-        return result
-
-'''
-# using priority queue
-class Solution:
-    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        count = Counter(nums)
-        priority_queue = []
+        counter = Counter(nums)
+        kFrequent = counter.most_common(k)
         result = []
-        
-        for key, value in count.items():
-            heapq.heappush(priority_queue, (-value, key))
-        
-        for _ in range(0, k):
-            priority, element = heapq.heappop(priority_queue)
-            result.append(element)
+
+        for ele in kFrequent:
+            result.append(ele[0])
 
         return result
-'''
