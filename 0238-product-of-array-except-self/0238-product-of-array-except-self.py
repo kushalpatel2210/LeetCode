c3c87c65e8cfd33d7dbab9e4f2777c22b53413fa
Kushal Patel <61028285+kushalpatel2210@users.noreply.github.com>
2025-01-28 18:37:31 -0500

    Time: 31 ms (35.87%), Space: 27.2 MB (9.82%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -1,24 +1,16 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        product_except_self, left_products, right_products = list(), list(), list()
-        left_product = right_product = 1
+        left_products = [1] * len(nums)
+        right_products = [1] * len(nums)
+        products = list()
 
-        for index, value in enumerate(nums):
-            if index == 0:
-                left_products.append(left_product)
-            else:
-                left_product *= nums[index - 1]
-                left_products.append(left_product)
+        for i in range(1, len(nums)):
+            left_products[i] = left_products[i - 1] * nums[i - 1]
         
-        for index in range(len(nums) -1, -1, -1):
-            if index == len(nums) - 1:
-                right_products.append(1)
-            else:
-                right_product *= nums[index + 1]
-                right_products.append(right_product)
-        right_products.reverse()
-
+        for i in range(len(nums) - 2, -1, -1):
+            right_products[i] = right_products[i + 1] * nums[i + 1]
+        
         for i in range(len(nums)):
-            product_except_self.append(left_products[i] * right_products[i])
-        
-        return product_except_self
+            products.append(left_products[i] * right_products[i])
+
+        return products
