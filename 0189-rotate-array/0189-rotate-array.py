a891d6256e06527a0d5d068eb37b041c629d94ea
Kushal Patel <61028285+kushalpatel2210@users.noreply.github.com>
2025-02-03 19:05:28 -0500

    Time: 3 ms (72%), Space: 25.7 MB (39.52%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.py
+++ 0189-rotate-array/0189-rotate-array.py
@@ -1,4 +1,22 @@
 class Solution:
+    def reverse(self, nums, l, r):
+        while l < r:
+            nums[l], nums[r] = nums[r], nums[l]
+            l, r = l + 1, r - 1
+
+    def rotate(self, nums: List[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        n = len(nums)
+        k = k % n
+
+        self.reverse(nums, 0, n -1)
+        self.reverse(nums, 0, k -1)
+        self.reverse(nums, k, n -1)
+
+'''
+class Solution:
     def rotate(self, nums: List[int], k: int) -> None:
         """
         Do not return anything, modify nums in-place instead.
@@ -12,3 +30,4 @@ class Solution:
             ans[index] = nums[i]
                 
         nums[:] = ans
+'''
