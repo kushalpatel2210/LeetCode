4bf4c067f1a60db4c8007dec70f9f55e86445768
Kushal Patel <61028285+kushalpatel2210@users.noreply.github.com>
2025-02-04 21:15:33 -0500

    Time: 33 ms (20.42%), Space: 26.2 MB (91.84%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.py
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.py
@@ -1,5 +1,21 @@
 class Solution:
     def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
+        L = 0 
+        window = set()
+
+        for R in range(len(nums)):
+            if R - L > k:
+                window.remove(nums[L])
+                L += 1
+            if nums[R] in window:
+                return True
+            window.add(nums[R])
+        
+        return False
+
+'''
+class Solution:
+    def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
         numbers = set()
         
         for i in range(len(nums)):
@@ -12,3 +28,4 @@ class Solution:
                 numbers.remove(nums[i - k])
         
         return False
+'''
