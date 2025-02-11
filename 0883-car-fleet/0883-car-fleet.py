ce7a9a7bacacd01c2f5f2eddd842a98b929be99a
Kushal Patel <61028285+kushalpatel2210@users.noreply.github.com>
2025-02-10 21:19:08 -0500

    Time: 215 ms (32.52%), Space: 37.5 MB (83.3%) - LeetHub

--- 0883-car-fleet/0883-car-fleet.py
+++ 0883-car-fleet/0883-car-fleet.py
@@ -5,8 +5,24 @@ class Solution:
 
         for pos, spd in cars:
             time = (target - pos) / spd
+            stack.append(time)
+
+            while len(stack) > 1 and stack[-1] <= stack[-2]:
+                stack.pop()
+        
+        return len(stack)
+
+'''
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        cars = sorted(zip(position, speed), reverse = True)
+        stack = []
+
+        for pos, spd in cars:
+            time = (target - pos) / spd
 
             if not stack or stack[-1] < time:
                 stack.append(time)
 
         return len(stack)
+'''
