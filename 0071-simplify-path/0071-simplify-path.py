cb12b5956bbf16d5da2d405defbb96ac5a36f7ee
Kushal Patel <61028285+kushalpatel2210@users.noreply.github.com>
2025-02-11 21:18:20 -0500

    Time: 3 ms (37.52%), Space: 17.9 MB (27.47%) - LeetHub

--- 0071-simplify-path/0071-simplify-path.py
+++ 0071-simplify-path/0071-simplify-path.py
@@ -1,3 +1,18 @@
+class Solution:
+    def simplifyPath(self, path: str) -> str:
+        stack = []
+        paths = path.split('/')
+
+        for curr in paths:
+            if curr == '..':
+                if stack:
+                    stack.pop()
+            elif curr != '' and curr != '.':
+                stack.append(curr)
+
+        return '/' + '/'.join(stack)
+
+'''
 import re
 
 class Solution:
@@ -28,3 +43,4 @@ class Solution:
         simplified += '/'.join(reversed(outputStack))
 
         return simplified
+'''
