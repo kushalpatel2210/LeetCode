6b9ba044b0884d6d52f80689387d23ce36c91453
Kushal Patel <61028285+kushalpatel2210@users.noreply.github.com>
2025-02-13 06:49:33 -0500

    Time: 9803 ms (5.02%), Space: 26.5 MB (38.6%) - LeetHub

---
+++ 0931-maximum-frequency-stack/0931-maximum-frequency-stack.py
@@ -0,0 +1,26 @@
+from collections import defaultdict
+class FreqStack:
+
+    def __init__(self):
+        self.stack = []
+        self.tempStack = []
+        self.hashMap = defaultdict(int)
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+        self.hashMap[val] += 1
+
+    def pop(self) -> int:
+        maxValue = max(self.hashMap.values())
+        maxElements = [key for key, val in self.hashMap.items() if val == maxValue]
+
+        for i in range(len(self.stack) - 1, -1 ,-1):
+            if self.stack[i] in maxElements:
+                maxFrq = self.stack.pop(i)
+                self.hashMap[maxFrq] -= 1
+                return maxFrq
+
+# Your FreqStack object will be instantiated and called as such:
+# obj = FreqStack()
+# obj.push(val)
+# param_2 = obj.pop()
