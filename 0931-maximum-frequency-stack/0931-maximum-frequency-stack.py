afe1b38fa5f47f0273f8d947263873f334b3c7ec
Kushal Patel <61028285+kushalpatel2210@users.noreply.github.com>
2025-02-13 06:52:37 -0500

    Time: 114 ms (18.64%), Space: 26.5 MB (38.6%) - LeetHub

--- 0931-maximum-frequency-stack/0931-maximum-frequency-stack.py
+++ 0931-maximum-frequency-stack/0931-maximum-frequency-stack.py
@@ -1,3 +1,21 @@
+class FreqStack:
+    
+    def __init__(self):
+        self.heap = []
+        self.cnt = defaultdict(int)
+        self.index = 0
+
+    def push(self, val: int) -> None:
+        self.cnt[val] += 1
+        heapq.heappush(self.heap, (-self.cnt[val], -self.index, val))
+        self.index += 1
+
+    def pop(self) -> int:
+        _, _, val = heapq.heappop(self.heap)
+        self.cnt[val] -= 1
+        return val
+
+''''
 from collections import defaultdict
 class FreqStack:
 
@@ -19,6 +37,7 @@ class FreqStack:
                 maxFrq = self.stack.pop(i)
                 self.hashMap[maxFrq] -= 1
                 return maxFrq
+'''
 
 # Your FreqStack object will be instantiated and called as such:
 # obj = FreqStack()
