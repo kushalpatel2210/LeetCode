734af30bf9c6b988aff557957847d296b54c40f3
Kushal Patel <61028285+kushalpatel2210@users.noreply.github.com>
2025-02-15 17:44:34 -0500

    Time: 159 ms (53.4%), Space: 18.8 MB (76.11%) - LeetHub

---
+++ 0907-koko-eating-bananas/0907-koko-eating-bananas.py
@@ -0,0 +1,23 @@
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        totalBananas = sum(piles)
+        minSpeed = math.ceil(totalBananas / h)
+        maxSpeed = max(piles)
+        l, r = minSpeed, maxSpeed
+        minEatSpeed = float('inf')
+
+        while l <= r:
+            m = l + (r - l) // 2
+            totalIterations = 0
+
+            for pile in piles:
+                totalIterations += math.ceil(pile / m)
+            
+            # print(f"totalIterations {totalIterations} l {l} r {r} mid {m}")
+            if totalIterations > h:
+                l = m + 1
+            else:
+                minEatSpeed = min(minEatSpeed, m)
+                r = m - 1 
+
+        return minEatSpeed
