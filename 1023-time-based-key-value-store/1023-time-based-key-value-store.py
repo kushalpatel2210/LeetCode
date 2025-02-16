fbdd273cb9bbd93f54b58d3b57d99386e0bab3f0
Kushal Patel <61028285+kushalpatel2210@users.noreply.github.com>
2025-02-16 14:34:08 -0500

    Time: 182 ms (11.2%), Space: 74 MB (91.03%) - LeetHub

--- 1023-time-based-key-value-store/1023-time-based-key-value-store.py
+++ 1023-time-based-key-value-store/1023-time-based-key-value-store.py
@@ -1,25 +1,3 @@
-from sortedcontainers import SortedDict
-
-class TimeMap:
-    def __init__(self):
-        self.m = defaultdict(SortedDict)
-
-    def set(self, key: str, value: str, timestamp: int) -> None:
-        self.m[key][timestamp] = value
-
-    def get(self, key: str, timestamp: int) -> str:
-        if key not in self.m:
-            return ""
-        
-        timestamps = self.m[key]
-        idx = timestamps.bisect_right(timestamp) - 1
-        
-        if idx >= 0:
-            closest_time = timestamps.iloc[idx]
-            return timestamps[closest_time]
-        return ""
-        
-'''
 from collections import defaultdict
 
 class TimeMap:
@@ -48,6 +26,29 @@ class TimeMap:
                 r = m - 1
 
         return element
+        
+'''
+from sortedcontainers import SortedDict
+
+class TimeMap:
+    def __init__(self):
+        self.m = defaultdict(SortedDict)
+
+    def set(self, key: str, value: str, timestamp: int) -> None:
+        self.m[key][timestamp] = value
+
+    def get(self, key: str, timestamp: int) -> str:
+        if key not in self.m:
+            return ""
+        
+        timestamps = self.m[key]
+        idx = timestamps.bisect_right(timestamp) - 1
+        
+        if idx >= 0:
+            closest_time = timestamps.iloc[idx]
+            return timestamps[closest_time]
+        return ""
+
 '''
 
 # Your TimeMap object will be instantiated and called as such:
