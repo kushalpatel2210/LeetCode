e80cb8d0e1757a5ed455e989bf79388f1444ee77
Kushal Patel <61028285+kushalpatel2210@users.noreply.github.com>
2025-02-16 14:15:18 -0500

    Time: 271 ms (5.08%), Space: 75.2 MB (37.7%) - LeetHub

--- 1023-time-based-key-value-store/1023-time-based-key-value-store.py
+++ 1023-time-based-key-value-store/1023-time-based-key-value-store.py
@@ -1,3 +1,25 @@
+from sortedcontainers import SortedDict
+
+class TimeMap:
+    def __init__(self):
+        self.m = defaultdict(SortedDict)
+
+    def set(self, key: str, value: str, timestamp: int) -> None:
+        self.m[key][timestamp] = value
+
+    def get(self, key: str, timestamp: int) -> str:
+        if key not in self.m:
+            return ""
+        
+        timestamps = self.m[key]
+        idx = timestamps.bisect_right(timestamp) - 1
+        
+        if idx >= 0:
+            closest_time = timestamps.iloc[idx]
+            return timestamps[closest_time]
+        return ""
+        
+'''
 from collections import defaultdict
 
 class TimeMap:
@@ -12,7 +34,7 @@ class TimeMap:
         l, r = 0, len(self.hashMap[key]) - 1 
         diff = float('inf')
         element = ""
-        
+
         while l <= r:
             m = l + (r - l) // 2
             val, time = self.hashMap[key][m]
@@ -26,6 +48,7 @@ class TimeMap:
                 r = m - 1
 
         return element
+'''
 
 # Your TimeMap object will be instantiated and called as such:
 # obj = TimeMap()
