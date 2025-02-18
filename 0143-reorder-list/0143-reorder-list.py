d4856a8b6d5c856e135020102df875f6bc92c8c3
Kushal Patel <61028285+kushalpatel2210@users.noreply.github.com>
2025-02-18 08:28:43 -0500

    Time: 7 ms (21.79%), Space: 23.4 MB (38.12%) - LeetHub

--- 0143-reorder-list/0143-reorder-list.py
+++ 0143-reorder-list/0143-reorder-list.py
@@ -3,6 +3,40 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+
+# time O(n) space O(1)
+class Solution:
+    def reorderList(self, head: Optional[ListNode]) -> None:
+        """
+        Do not return anything, modify head in-place instead.
+        """
+        # find middle
+        slow, fast = head, head.next
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        
+        second = slow.next
+        slow.next = None
+
+        # reverse the second half
+        prev = None
+        while second:
+            tmp = second.next
+            second.next = prev
+            prev = second 
+            second = tmp
+        
+        # merge two halfs 
+        first, second = head, prev
+        while second:
+            tmp1, tmp2 = first.next, second.next
+            first.next = second
+            second.next = tmp1
+            first, second = tmp1, tmp2 
+
+'''
+# time and space O(n)
 class Solution:
     def reorderList(self, head: Optional[ListNode]) -> None:
         """
@@ -25,3 +59,4 @@ class Solution:
             curr = pop.next
         
         stack.pop().next = None
+'''
