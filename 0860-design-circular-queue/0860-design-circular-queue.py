31801ddab2ce1159bd3d16acfd53d06f60533283
Kushal Patel <61028285+kushalpatel2210@users.noreply.github.com>
2025-02-22 15:15:04 -0500

    Time: 5 ms (71.02%), Space: 18.4 MB (75.56%) - LeetHub

--- 0860-design-circular-queue/0860-design-circular-queue.py
+++ 0860-design-circular-queue/0860-design-circular-queue.py
@@ -1,3 +1,65 @@
+class ListNode:
+    def __init__(self, val = 0, nxt = None):
+        self.val = val
+        self.next = nxt
+
+class MyCircularQueue:
+    def __init__(self, k: int):
+        self.space = k 
+        self.left = ListNode()
+        self.right = self.left
+
+    def enQueue(self, value: int) -> bool:
+        if self.isFull(): 
+            return False
+        
+        curr = ListNode(value)
+        if self.isEmpty():
+            self.left.next = curr
+            self.right = curr
+        else:
+            self.right.next = curr 
+            self.right = curr
+        
+        self.space -= 1
+        return True
+
+    def deQueue(self) -> bool:
+        if self.isEmpty():
+            return False
+        
+        self.left.next = self.left.next.next
+        if self.left.next is None:
+            self.right = self.left
+        
+        self.space += 1
+        return True
+
+    def Front(self) -> int:
+        if self.isEmpty(): return -1
+        return self.left.next.val
+
+    def Rear(self) -> int:
+        if self.isEmpty(): return -1
+        return self.right.val
+
+    def isEmpty(self) -> bool:
+        return self.left.next == None
+
+    def isFull(self) -> bool:
+        return self.space == 0
+
+
+# Your MyCircularQueue object will be instantiated and called as such:
+# obj = MyCircularQueue(k)
+# param_1 = obj.enQueue(value)
+# param_2 = obj.deQueue()
+# param_3 = obj.Front()
+# param_4 = obj.Rear()
+# param_5 = obj.isEmpty()
+# param_6 = obj.isFull()
+
+'''
 class MyCircularQueue:
 
     def __init__(self, k: int):
@@ -27,14 +89,4 @@ class MyCircularQueue:
 
     def isFull(self) -> bool:
         return len(self.queue) == self.k
-        
-
-
-# Your MyCircularQueue object will be instantiated and called as such:
-# obj = MyCircularQueue(k)
-# param_1 = obj.enQueue(value)
-# param_2 = obj.deQueue()
-# param_3 = obj.Front()
-# param_4 = obj.Rear()
-# param_5 = obj.isEmpty()
-# param_6 = obj.isFull()
+'''
