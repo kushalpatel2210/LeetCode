9363e58b36bb3e560a28560f9f28ce8819ba390b
Kushal Patel <61028285+kushalpatel2210@users.noreply.github.com>
2025-03-02 13:12:02 -0500

    Time: 15 ms (92.25%), Space: 28.9 MB (71.13%) - LeetHub

--- 0932-monotonic-array/0932-monotonic-array.py
+++ 0932-monotonic-array/0932-monotonic-array.py
@@ -1,4 +1,22 @@
 class Solution:
     def isMonotonic(self, nums: List[int]) -> bool:
+        n = len(nums)
+        isIncreasing = nums[0] <= nums[n - 1]
+        
+        for i in range(1, n):
+            if isIncreasing:
+                if nums[i] < nums[i -1]:
+                    return False
+            else:
+                if nums[i] > nums[i- 1]:
+                    return False
+        
+        return True
+
+'''
+Time Complexity = O(nlogn)
+class Solution:
+    def isMonotonic(self, nums: List[int]) -> bool:
         sortedAsc, sortedDesc = sorted(nums), sorted(nums, reverse = True)
         return True if nums == sortedAsc or nums == sortedDesc else False
+'''
