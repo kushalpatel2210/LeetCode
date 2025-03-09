f84552785646035a552a06194e80819868b67d3b
Kushal Patel <61028285+kushalpatel2210@users.noreply.github.com>
2025-03-09 17:58:34 -0400

    Time: 3 ms (66.75%), Space: 18.5 MB (11.88%) - LeetHub

---
+++ 1184-car-pooling/1184-car-pooling.py
@@ -0,0 +1,24 @@
+class Solution:
+    def carPooling(self, trips: List[List[int]], capacity: int) -> bool:
+        passengerTrips =[]
+
+        for trip in trips:
+            passengers, start, end = trip
+            heapq.heappush(passengerTrips, (start, end, passengers))
+        
+        destinations = [] # (end, passengers)
+        while passengerTrips:
+            start, end, passengers = heapq.heappop(passengerTrips)
+            # print(f"start {start} end {end} passengers {passengers} capacity {capacity}")
+            
+            while destinations and destinations[0][0] <= start:
+                _, cap = heapq.heappop(destinations)
+                capacity += cap
+            
+            if passengers <= capacity:
+                heapq.heappush(destinations, (end, passengers))
+                capacity -= passengers
+            else:
+                return False
+
+        return True
