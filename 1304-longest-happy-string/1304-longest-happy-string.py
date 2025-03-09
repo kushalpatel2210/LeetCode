51951fd725cd13d238be7bf494d1a7e573bd98d8
Kushal Patel <61028285+kushalpatel2210@users.noreply.github.com>
2025-03-09 17:38:02 -0400

    Time: 0 ms (100%), Space: 18.1 MB (24.94%) - LeetHub

--- 1304-longest-happy-string/1304-longest-happy-string.py
+++ 1304-longest-happy-string/1304-longest-happy-string.py
@@ -1,6 +1,46 @@
 class Solution:
     def longestDiverseString(self, a: int, b: int, c: int) -> str:
         maxHeap = []
+        for count, char in [(-a, "a"), (-b, "b"), (-c, "c")]:
+            if count != 0:
+                heapq.heappush(maxHeap, (count, char))
+        
+        res = ""
+        while maxHeap:
+            n = len(res)
+
+            if n < 2:
+                count, char = heapq.heappop(maxHeap)
+                res += char
+                count += 1
+                if count:
+                    heapq.heappush(maxHeap, (count, char))
+            else:
+                first, second = res[n - 1], res[n - 2]
+                count, char = heapq.heappop(maxHeap)
+                
+                if first == second == char: 
+                    if not maxHeap:
+                        break
+                    count2, char2 = heapq.heappop(maxHeap)
+                    res += char2
+                    count2 += 1
+                    if count2:
+                        heapq.heappush(maxHeap, (count2, char2))   
+                    heapq.heappush(maxHeap, (count, char))
+                else:
+                    res += char
+                    count += 1
+                    if count:
+                        heapq.heappush(maxHeap, (count, char))
+
+        return res  
+
+
+'''
+class Solution:
+    def longestDiverseString(self, a: int, b: int, c: int) -> str:
+        maxHeap = []
         if a != 0:
             heapq.heappush(maxHeap, (-a, 'a'))
         if b != 0:
@@ -40,3 +80,4 @@ class Solution:
                     return res
 
         return res  
+'''
