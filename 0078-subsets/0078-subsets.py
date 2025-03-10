d1dc427a743704e3d1b60a4e4e812d04d872b2e9
Kushal Patel <61028285+kushalpatel2210@users.noreply.github.com>
2025-03-10 08:04:27 -0400

    Time: 0 ms (100%), Space: 18.1 MB (21.2%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -1,6 +1,23 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
         res = []
+
+        def backtrack(currentPath, index):
+            res.append(currentPath.copy())
+
+            for i in range(index, len(nums)):
+                currentPath.append(nums[i]) #include
+                backtrack(currentPath, i + 1) # explore
+                currentPath.pop() # exclude
+        
+        backtrack([], 0)
+        return res
+
+
+'''
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        res = []
         subset = []
 
         def dfs(i):
@@ -16,21 +33,5 @@ class Solution:
             dfs(i + 1)
         
         dfs(0)
-        return res
-
-'''
-class Solution:
-    def subsets(self, nums: List[int]) -> List[List[int]]:
-        res = []
-
-        def backtrack(currentPath, index):
-            res.append(currentPath[:])
-
-            for i in range(index, len(nums)):
-                currentPath.append(nums[i]) #include
-                backtrack(currentPath, i + 1) # explore
-                currentPath.pop() # exclude
-        
-        backtrack([], 0)
         return res
 '''
