31e541d6ab66ca58dfaa1469557581721f1bdb57
Kushal Patel <61028285+kushalpatel2210@users.noreply.github.com>
2025-03-21 07:35:05 -0400

    Time: 2 ms (56.02%), Space: 18 MB (9.67%) - LeetHub

--- 1666-make-the-string-great/1666-make-the-string-great.py
+++ 1666-make-the-string-great/1666-make-the-string-great.py
@@ -10,6 +10,5 @@ class Solution:
                     stack.pop()
                 else: 
                     stack.append(char)
-        print(stack)
-                
+                                    
         return "".join(stack)
