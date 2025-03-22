555a0adf0b3c06a3548b8d9a169ae465b463de23
Kushal Patel <61028285+kushalpatel2210@users.noreply.github.com>
2025-03-22 15:30:53 -0400

    Time: 0 ms (100%), Space: 24.8 MB (45.76%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.py
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.py
@@ -1,8 +1,28 @@
 class Solution:
     def singleNonDuplicate(self, nums: List[int]) -> int:
+        l, r = 0, len(nums) - 1
+
+        while l <= r:
+            m = l + (r - l) // 2
+
+            if ((m - 1 < 0 or nums[m - 1] != nums[m]) and (m + 1 == len(nums) or nums[m + 1] != nums[m])):
+                return nums[m]
+            
+            leftSize = m - 1 if nums[m - 1] == nums[m] else m
+            if leftSize % 2:
+                r = m - 1
+            else:
+                l = m + 1
+
+
+'''
+# Bitwise XOR
+class Solution:
+    def singleNonDuplicate(self, nums: List[int]) -> int:
         xor = 0
 
         for num in nums:
             xor ^= num
         
         return xor
+'''
