8e964289c3a7322ad81532c543c05dc2263a487a
Kushal Patel <61028285+kushalpatel2210@users.noreply.github.com>
2025-03-25 07:11:02 -0400

    Time: 0 ms (100%), Space: 17.8 MB (39.47%) - LeetHub

--- 2346-largest-3-same-digit-number-in-string/2346-largest-3-same-digit-number-in-string.py
+++ 2346-largest-3-same-digit-number-in-string/2346-largest-3-same-digit-number-in-string.py
@@ -1,3 +1,14 @@
+class Solution:
+    def largestGoodInteger(self, num: str) -> str:
+        res = -1
+        
+        for i in range(len(num) - 2):
+            if num[i] == num[i + 1] == num[i + 2]:
+                res = max(res, int(num[i]))
+                
+        return str(res) * 3 if res != -1 else ""
+
+'''
 from collections import Counter 
 
 class Solution:
@@ -23,3 +34,4 @@ class Solution:
             l += 1
         
         return result
+'''
