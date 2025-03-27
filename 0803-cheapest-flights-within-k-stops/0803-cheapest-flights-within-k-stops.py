13f7a5b9b4c02b2436e20543fbf675fd494270d4
Kushal Patel <61028285+kushalpatel2210@users.noreply.github.com>
2025-03-27 07:58:03 -0400

    Time: 5 ms (72.73%), Space: 19.1 MB (70.51%) - LeetHub

--- 0803-cheapest-flights-within-k-stops/0803-cheapest-flights-within-k-stops.py
+++ 0803-cheapest-flights-within-k-stops/0803-cheapest-flights-within-k-stops.py
@@ -2,23 +2,24 @@ class Solution:
     def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
         adj = defaultdict(list)
         prices = [float('inf')] * n
-        prices[src] = 0
+        # prices[src] = 0
 
-        for source, dest, price in flights:
-            adj[source].append((price, dest)) # weight, destination
-
-        q = deque([(0, src, 0)]) # price, dest, stops
+        for source, destination, cost in flights:
+            adj[source].append((destination, cost)) # node, weight
+        
+        q = deque([(0, src, 0)]) # (cost, destination, stops)
 
         while q:
-            p, d, s = q.popleft()
+            cost, destination, stops = q.popleft()
 
-            if s > k:
+            if stops > k:
                 continue
-
-            for p2, d2 in adj[d]:
-                nextCost = p + p2
-                if nextCost < prices[d2]:
-                    prices[d2] = nextCost
-                    q.append((nextCost, d2, s + 1))
+            
+            for destination2, cost2 in adj[destination]:
+                newCost = cost + cost2
+                if newCost < prices[destination2]:
+                    prices[destination2] = newCost
+                    q.append((newCost, destination2, stops + 1))
+        
 
         return prices[dst] if prices[dst] != float('inf') else -1
