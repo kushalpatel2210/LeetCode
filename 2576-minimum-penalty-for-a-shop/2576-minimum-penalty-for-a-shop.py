8990986284dc54f333be5aa1a579012be46c4d3e
Kushal Patel <61028285+kushalpatel2210@users.noreply.github.com>
2025-03-26 21:53:59 -0400

    Time: 39 ms (98.9%), Space: 17.9 MB (93.89%) - LeetHub

--- 2576-minimum-penalty-for-a-shop/2576-minimum-penalty-for-a-shop.py
+++ 2576-minimum-penalty-for-a-shop/2576-minimum-penalty-for-a-shop.py
@@ -1,5 +1,16 @@
 class Solution:
     def bestClosingTime(self, customers: str) -> int:
+        max_score = 0
+        score = 0
+        right_time = -1
+        for i in range(len(customers)):
+            score += 1 if customers[i] == 'Y' else -1
+            if score > max_score:
+                max_score = score
+                right_time = i
+        return right_time + 1
+
+'''
         no_of_y = customers.count('Y')
         no_of_n = 0
         penalty = float('inf')
@@ -19,3 +30,4 @@ class Solution:
                     no_of_n += 1
                 
         return res
+'''
