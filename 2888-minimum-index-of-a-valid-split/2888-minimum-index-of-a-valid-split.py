6fc0c971ec79a8be9f0cdab944b2843bc5e84c00
Kushal Patel <61028285+kushalpatel2210@users.noreply.github.com>
2025-03-26 21:17:30 -0400

    Time: 51 ms (82.18%), Space: 34.6 MB (38.61%) - LeetHub

--- 2888-minimum-index-of-a-valid-split/2888-minimum-index-of-a-valid-split.py
+++ 2888-minimum-index-of-a-valid-split/2888-minimum-index-of-a-valid-split.py
@@ -1,5 +1,28 @@
 class Solution:
     def minimumIndex(self, nums: List[int]) -> int:
+        counter = Counter(nums)
+        most_common = counter.most_common(1)[0][0]
+        left = 0
+        right = counter.most_common(1)[0][1]
+        
+        for i in range(len(nums)):
+            if nums[i] == most_common:
+                left += 1
+                right -= 1
+            
+                left_size = i + 1
+                right_size = len(nums) - i - 1
+
+                if 2 * left > left_size and 2 * right > right_size:
+                    return i
+        
+        return -1
+
+
+
+'''
+class Solution:
+    def minimumIndex(self, nums: List[int]) -> int:
         left = defaultdict(int)
         right = Counter(nums)
 
@@ -14,3 +37,4 @@ class Solution:
                 return i
         
         return -1
+'''
