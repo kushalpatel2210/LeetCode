1aaa0b2562a601b4787d40808edbb1d98316238e
Kushal Patel <61028285+kushalpatel2210@users.noreply.github.com>
2025-03-27 07:33:20 -0400

    Time: 55 ms (69.28%), Space: 18.2 MB (63.64%) - LeetHub

--- 2576-minimum-penalty-for-a-shop/2576-minimum-penalty-for-a-shop.py
+++ 2576-minimum-penalty-for-a-shop/2576-minimum-penalty-for-a-shop.py
@@ -1,33 +1,20 @@
 class Solution:
     def bestClosingTime(self, customers: str) -> int:
-        max_score = 0
-        score = 0
-        right_time = -1
-        for i in range(len(customers)):
-            score += 1 if customers[i] == 'Y' else -1
-            if score > max_score:
-                max_score = score
-                right_time = i
-        return right_time + 1
-
-'''
         no_of_y = customers.count('Y')
         no_of_n = 0
-        penalty = float('inf')
+        penalty = no_of_y
         res = 0
 
-        for i in range(len(customers) + 1):
+        for i in range(len(customers)):
+            if customers[i] == 'Y':
+                no_of_y -= 1
+            else:
+                no_of_n += 1
+
             curr_penalty = no_of_y + no_of_n
             
             if curr_penalty < penalty:
-                res = i
+                res = i + 1
                 penalty = curr_penalty
 
-            if i < len(customers):
-                if customers[i] == 'Y':
-                    no_of_y -= 1
-                else:
-                    no_of_n += 1
-                
         return res
-'''
