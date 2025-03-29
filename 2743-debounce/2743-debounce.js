67412587f8fa4c269ca6a3b1a8faadb661907c59
Kushal Patel <61028285+kushalpatel2210@users.noreply.github.com>
2025-03-29 19:53:08 -0400

    Time: 51 ms (58.4%), Space: 53.6 MB (49.1%) - LeetHub

--- 2743-debounce/2743-debounce.js
+++ 2743-debounce/2743-debounce.js
@@ -3,14 +3,14 @@
  * @param {number} t milliseconds
  * @return {Function}
  */
+
 var debounce = function(fn, t) {
     let timer
     return function(...args) {
         clearTimeout(timer)
         timer = setTimeout(() => fn(...args), t)
     }
-};
-
+}
 /**
  * const log = debounce(console.log, 100);
  * log('Hello'); // cancelled
