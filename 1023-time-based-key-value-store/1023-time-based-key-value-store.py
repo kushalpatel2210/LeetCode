820f72deb8fd5027b1ccebfdc058f203f7c7f690
Kushal Patel <61028285+kushalpatel2210@users.noreply.github.com>
2025-03-29 18:35:37 -0400

    Time: 76 ms (87.76%), Space: 74.1 MB (88.28%) - LeetHub

--- 1023-time-based-key-value-store/1023-time-based-key-value-store.py
+++ 1023-time-based-key-value-store/1023-time-based-key-value-store.py
@@ -3,53 +3,22 @@ from collections import defaultdict
 class TimeMap:
 
     def __init__(self):
-        self.hashMap = defaultdict(list)
+        self.map = defaultdict(list)
+        
 
     def set(self, key: str, value: str, timestamp: int) -> None:
-       self.hashMap[key].append((value, timestamp))
+        self.map[key].append((value, timestamp))
+        
 
     def get(self, key: str, timestamp: int) -> str:
-        l, r = 0, len(self.hashMap[key]) - 1 
-        diff = float('inf')
-        element = ""
-
-        while l <= r:
-            m = l + (r - l) // 2
-            val, time = self.hashMap[key][m]
-            
-            if time <= timestamp and timestamp - time < diff:
-                element = val
-            
-            if time < timestamp:
-                l = m + 1
-            else:
-                r = m - 1
-
-        return element
+        if key in self.map:
+            values = self.map[key]
+            for i in range(len(values) - 1, -1 , -1):
+                if values[i][1] <= timestamp:
+                    return values[i][0]
         
-'''
-from sortedcontainers import SortedDict
-
-class TimeMap:
-    def __init__(self):
-        self.m = defaultdict(SortedDict)
-
-    def set(self, key: str, value: str, timestamp: int) -> None:
-        self.m[key][timestamp] = value
-
-    def get(self, key: str, timestamp: int) -> str:
-        if key not in self.m:
-            return ""
-        
-        timestamps = self.m[key]
-        idx = timestamps.bisect_right(timestamp) - 1
-        
-        if idx >= 0:
-            closest_time = timestamps.iloc[idx]
-            return timestamps[closest_time]
         return ""
 
-'''
 
 # Your TimeMap object will be instantiated and called as such:
 # obj = TimeMap()
