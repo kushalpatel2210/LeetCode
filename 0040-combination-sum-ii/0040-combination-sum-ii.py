ade3e76a5cbd7e1e53c3ffba068c83baaa817032
Kushal Patel <61028285+kushalpatel2210@users.noreply.github.com>
2025-03-30 12:38:38 -0400

    Time: 27 ms (35.77%), Space: 17.8 MB (83.8%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.py
+++ 0040-combination-sum-ii/0040-combination-sum-ii.py
@@ -3,23 +3,24 @@ class Solution:
         candidates.sort()
         res = []
 
-        def dfs(i, currPath, currSum):
-            if currSum == target:
+        def dfs(i, currPath, total):
+            if total == target:
                 res.append(currPath.copy())
-                return
-            if i >= len(candidates) or currSum > target:
-                return
+                return 
+            
+            if total > target or i >= len(candidates):
+                return 
             
             currElement = candidates[i]
             currPath.append(currElement)
-            dfs(i + 1, currPath, currSum + currElement)
+            dfs(i + 1, currPath, total + currElement)
             currPath.pop()
 
             while i < len(candidates) and candidates[i] == currElement:
                 i += 1
-
-            dfs(i, currPath, currSum)
+            
+            dfs(i, currPath, total)
         
         dfs(0, [], 0)
-
+    
         return res
