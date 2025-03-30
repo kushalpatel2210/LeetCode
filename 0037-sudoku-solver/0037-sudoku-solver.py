d4b3717732eb854770c0b1ec9fbb88a6e7f62317
Kushal Patel <61028285+kushalpatel2210@users.noreply.github.com>
2025-03-30 15:43:40 -0400

    Time: 479 ms (72.18%), Space: 17.9 MB (86.05%) - LeetHub

--- 0037-sudoku-solver/0037-sudoku-solver.py
+++ 0037-sudoku-solver/0037-sudoku-solver.py
@@ -1,57 +1,66 @@
+EMPTY_CELL = "."
+
+class SudokuBoard:
+    def __init__(self, board: list[list[str]]):
+        self.board = board
+
+        self._row_used = defaultdict(set)
+        self._col_used = defaultdict(set)
+        self._box_used = defaultdict(set)
+
+        for i in range(len(board)):
+            for j in range(len(board[i])):
+                if (num := board[i][j]) != EMPTY_CELL:
+                    self._row_used[i].add(num)
+                    self._col_used[j].add(num)
+                    self._box_used[i // 3, j // 3].add(num)
+
+    def add(self, i: int, j: int, num: str) -> None:
+        self.board[i][j] = num
+        self._row_used[i].add(num)
+        self._col_used[j].add(num)
+        self._box_used[i // 3, j // 3].add(num)
+
+    def remove(self, i: int, j: int) -> None:
+        num = self.board[i][j]
+        self.board[i][j] = EMPTY_CELL
+        self._row_used[i].remove(num)
+        self._col_used[j].remove(num)
+        self._box_used[i // 3, j // 3].remove(num)
+
+    def empty_cells(self) -> list[tuple[int, int, int]]:
+        empty_cells = []
+        for i in range(len(self.board)):
+            for j in range(len(self.board[i])):
+                if self.board[i][j] == EMPTY_CELL:
+                    heapq.heappush(empty_cells, (len(self.candidates(i, j)), i, j))
+        return empty_cells
+
+    def candidates(self, i: int, j: int) -> set[str]:
+        candidates = set(str(num) for num in range(1, 10)) - (
+            self._row_used[i] | self._col_used[j] | self._box_used[i // 3, j // 3]
+        )
+        return candidates
+
+
+def solve_sudoku(board: SudokuBoard) -> bool:
+    if not (empty_cells := board.empty_cells()):
+        return True
+
+    _, row, col = empty_cells[0]
+    candidates = board.candidates(row, col)
+    for candidate in candidates:
+        board.add(row, col, candidate)
+        if solve_sudoku(board):
+            return True
+        board.remove(row, col)
+    return False
+
+
 class Solution:
-    def solveSudoku(self, board: List[List[str]]) -> None:
+    def solveSudoku(self, board: list[list[str]]) -> None:
         """
-        This function solves a Sudoku puzzle using Depth First Search (DFS).
-        The given board is modified in-place to fill in the Sudoku solution.
+        Do not return anything, modify board in-place instead.
         """
-
-        def dfs(position: int):
-            # Base case: if all empty positions are filled, set completion flag to True
-            if position == len(empty_positions):
-                self.is_solved = True
-                return
-          
-            # Get the next position from the list of empty positions
-            row_index, col_index = empty_positions[position]
-
-            # Try placing all possible values (1-9) in the current empty cell
-            for value in range(9):
-                if (not rows_used[row_index][value] and 
-                        not cols_used[col_index][value] and 
-                        not blocks_used[row_index // 3][col_index // 3][value]):
-                  
-                    # Mark the value as used in the row, column, and 3x3 block
-                    rows_used[row_index][value] = cols_used[col_index][value] = blocks_used[row_index // 3][col_index // 3][value] = True
-                    board[row_index][col_index] = str(value + 1)
-
-                    # Recursively try to solve the rest of the puzzle
-                    dfs(position + 1)
-
-                    # Undo the move if it didn't lead to a solution
-                    rows_used[row_index][value] = cols_used[col_index][value] = blocks_used[row_index // 3][col_index // 3][value] = False
-
-                # If puzzle is solved, exit early
-                if self.is_solved:
-                    return
-
-        # Initialize tracking for used numbers in each row, column and block
-        rows_used = [[False] * 9 for _ in range(9)]
-        cols_used = [[False] * 9 for _ in range(9)]
-        blocks_used = [[[False] * 9 for _a in range(3)] for _b in range(3)]
-
-        empty_positions = []  # List to track the positions of empty cells
-        self.is_solved = False  # Flag to indicate when the puzzle is solved
-
-        # Process the Sudoku board to fill tracking structures and find empty cells
-        for row in range(9):
-            for col in range(9):
-                if board[row][col] == '.':
-                    # Save the position of the empty cell
-                    empty_positions.append((row, col))
-                else:
-                    # Mark the value as used in the row, column, and block
-                    value = int(board[row][col]) - 1
-                    rows_used[row][value] = cols_used[col][value] = blocks_used[row // 3][col // 3][value] = True
-
-        # Start the recursive DFS to solve the Sudoku
-        dfs(0)
+        solve_sudoku(SudokuBoard(board))
+        
