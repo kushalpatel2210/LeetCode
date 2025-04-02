a27ec39ca999a7f731f8c3a7a11073dc4c8258e5
Kushal Patel <61028285+kushalpatel2210@users.noreply.github.com>
2025-04-01 23:28:46 -0400

    Time: 10 ms (93.89%), Space: 19.5 MB (34.46%) - LeetHub

--- 1905-design-authentication-manager/1905-design-authentication-manager.py
+++ 1905-design-authentication-manager/1905-design-authentication-manager.py
@@ -1,36 +1,57 @@
-import heapq
+class ListNode():
+    def __init__(self, tokenId='', expiryTime=0):
+        self.tokenId, self.expiryTime = tokenId, expiryTime
+        self.next = self.prev = None
 
 class AuthenticationManager:
 
     def __init__(self, timeToLive: int):
-        self.authManager = dict() # tokenId, expiry
-        self.minHeap = [] #(expiryTime, tokenId)
+        self.authManager = dict()  # tokenId -> reference to node
+        self.start, self.end = ListNode(), ListNode()
+        self.start.next, self.end.prev = self.end, self.start  # Link dummy nodes
+        self.size = 0
         self.ttl = timeToLive
 
+    def insert(self, node):
+        prev, end = self.end.prev, self.end
+        prev.next, end.prev = node, node
+        node.prev, node.next = prev, end
+        self.size += 1
 
+    def remove(self, node):
+        prev, nxt = node.prev, node.next
+        prev.next, nxt.prev = nxt, prev
+        self.size -= 1
+        
     def generate(self, tokenId: str, currentTime: int) -> None:
-        tokenExpiry = currentTime + self.ttl
-        self.authManager[tokenId] = tokenExpiry
-        heapq.heappush(self.minHeap, (tokenExpiry, tokenId))
+        expiry = currentTime + self.ttl
+        self.authManager[tokenId] = ListNode(tokenId, expiry)
+        self.insert(self.authManager[tokenId])
 
     def renew(self, tokenId: str, currentTime: int) -> None:
         if tokenId in self.authManager:
-            currExpiryOfToken = self.authManager[tokenId]
-            if currExpiryOfToken > currentTime:
-                newExpiry = currentTime + self.ttl
-                self.authManager[tokenId] = newExpiry
-                heapq.heappush(self.minHeap, (newExpiry, tokenId))
+            node = self.authManager[tokenId]
+            if node.expiryTime > currentTime:
+                # Remove node from current position
+                self.remove(node)
+                # Update expiry time
+                node.expiryTime = currentTime + self.ttl
+                # Reinsert at the end
+                self.insert(node)
+            else:
+                # If expired, remove from map
+                self.remove(node)
+                del self.authManager[tokenId]
 
     def countUnexpiredTokens(self, currentTime: int) -> int:
-        while self.minHeap and self.minHeap[0][0] <= currentTime:
-            expiry, tokenId = heapq.heappop(self.minHeap)
-            if tokenId in self.authManager and self.authManager[tokenId] <= currentTime:
-                del self.authManager[tokenId]
-        
-        return len(self.authManager)
+        start = self.start.next
 
+        while start != self.end and start.expiryTime <= currentTime:
+            self.remove(start)
+            del self.authManager[start.tokenId]  # Also remove from dictionary
+            start = start.next  # Move to the next node
         
-
+        return self.size
 
 # Your AuthenticationManager object will be instantiated and called as such:
 # obj = AuthenticationManager(timeToLive)
