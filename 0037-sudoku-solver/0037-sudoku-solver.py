003f94b6ea67b8268ef7c4bec768b30d92a4e6a6
Kushal Patel <61028285+kushalpatel2210@users.noreply.github.com>
2025-04-02 22:03:31 -0400

    Time: 2355 ms (27.07%), Space: 18.1 MB (42.97%) - LeetHub

--- 0037-sudoku-solver/0037-sudoku-solver.py
+++ 0037-sudoku-solver/0037-sudoku-solver.py
@@ -1,46 +1,53 @@
 from collections import defaultdict
 
 class Solution:
-    def initialize(self,board): 
+    def solveSudoku(self, board: List[List[str]]) -> None:
+        self.board = board
+        self.row= defaultdict(set)
+        self.col= defaultdict(set)
+        self.box= defaultdict(set)
+        self.empty=[]
+        self.initialize()
+        self.solve(0)
+
+    def initialize(self): 
         for r in range(9):
             for c in range(9):
-                val = board[r][c]
-                if board[r][c] != ".":
+                val = self.board[r][c]
+                if val != ".":
                     self.row[r].add(val)
                     self.col[c].add(val)
                     self.box[(r //3, c // 3)].add(val)
                 else:
                     self.empty.append((r,c))
+    
+    def add(self, r, c, val):
+        self.board[r][c] = val
+        self.row[r].add(val)
+        self.col[c].add(val)
+        self.box[(r // 3, c // 3)].add(val)
+    
+    def remove(self, r, c, val):
+        self.board[r][c] = '.'
+        self.row[r].remove(val)
+        self.col[c].remove(val)
+        self.box[(r // 3, c // 3)].remove(val)
 
-    def solve(self,board,index):
-        if index==len(self.empty):
+    def solve(self, index):
+        if index == len(self.empty):
             return True
-        i,j=self.empty[index]
-
         
-        for c in "123456789":
-            if self.isSafe(i,j,c):
-                board[i][j]=c
-                self.row[i].add(c)
-                self.col[j].add(c)
-                self.box[(i // 3, j // 3)].add(c)
-                if self.solve(board,index+1):
+        r, c = self.empty[index]
+
+        for val in "123456789":
+            if self.isSafe(r, c, val):
+                self.add(r, c, val)
+                if self.solve(index + 1):
                     return True
-                board[i][j]="."
-                self.row[i].remove(c)
-                self.col[j].remove(c)
-                self.box[(i // 3, j // 3)].remove(c)
+                self.remove(r, c, val)
         return False
                         
-    def isSafe(self,i,j,c):
-        if c in self.row[i] or c in self.col[j] or c in self.box[(i // 3, j // 3)]:
+    def isSafe(self,r,c,val):
+        if val in self.row[r] or val in self.col[c] or val in self.box[(r // 3, c // 3)]:
             return False
         return True
-
-    def solveSudoku(self, board: List[List[str]]) -> None:
-        self.row= defaultdict(set)
-        self.col= defaultdict(set)
-        self.box= defaultdict(set)
-        self.empty=[]
-        self.initialize(board)
-        self.solve(board,0)
