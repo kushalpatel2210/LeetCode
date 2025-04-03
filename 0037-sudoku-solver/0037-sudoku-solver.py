de46fe143622aa7ea68362687657e4a75435a279
Kushal Patel <61028285+kushalpatel2210@users.noreply.github.com>
2025-04-02 21:50:43 -0400

    Time: 2218 ms (31.85%), Space: 17.8 MB (95.89%) - LeetHub

--- 0037-sudoku-solver/0037-sudoku-solver.py
+++ 0037-sudoku-solver/0037-sudoku-solver.py
@@ -1,14 +1,17 @@
+from collections import defaultdict
+
 class Solution:
-    def initialize(self,board):
-        
-        for i in range(9):
-            for j in range(9):
-                if board[i][j]!=".":
-                    self.row[i].add(board[i][j])
-                    self.col[j].add(board[i][j])
-                    self.box[(i//3)*3+(j//3)].add(board[i][j])
+    def initialize(self,board): 
+        for r in range(9):
+            for c in range(9):
+                val = board[r][c]
+                if board[r][c] != ".":
+                    self.row[r].add(val)
+                    self.col[c].add(val)
+                    self.box[(r //3, c // 3)].add(val)
                 else:
-                    self.empty.append((i,j))
+                    self.empty.append((r,c))
+
     def solve(self,board,index):
         if index==len(self.empty):
             return True
@@ -20,23 +23,24 @@ class Solution:
                 board[i][j]=c
                 self.row[i].add(c)
                 self.col[j].add(c)
-                self.box[(i//3)*3+(j//3)].add(c)
+                self.box[(i // 3, j // 3)].add(c)
                 if self.solve(board,index+1):
                     return True
                 board[i][j]="."
                 self.row[i].remove(c)
                 self.col[j].remove(c)
-                self.box[(i//3)*3+(j//3)].remove(c)
+                self.box[(i // 3, j // 3)].remove(c)
         return False
                         
     def isSafe(self,i,j,c):
-        if c in self.row[i] or c in self.col[j] or c in self.box[(i//3)*3+(j//3)]:
+        if c in self.row[i] or c in self.col[j] or c in self.box[(i // 3, j // 3)]:
             return False
         return True
+
     def solveSudoku(self, board: List[List[str]]) -> None:
-        self.row=[set() for _ in range(9)]
-        self.col=[set() for _ in range(9)]
-        self.box=[set() for _ in range(9)]
+        self.row= defaultdict(set)
+        self.col= defaultdict(set)
+        self.box= defaultdict(set)
         self.empty=[]
         self.initialize(board)
         self.solve(board,0)
