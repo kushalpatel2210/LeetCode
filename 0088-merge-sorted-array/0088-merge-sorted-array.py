45d0a85f76074442cdfb64eb9d306b19bc2e7394
Kushal Patel <61028285+kushalpatel2210@users.noreply.github.com>
2025-04-04 07:33:14 -0400

    Time: 0 ms (100%), Space: 17.6 MB (99.29%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -3,14 +3,14 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-        index_m, index_n = m - 1, n - 1
-        ptr = m + n - 1
+        last = m + n - 1
+        i, j = m - 1, n - 1
 
-        while index_n >= 0:
-            if index_m >= 0 and nums1[index_m] >= nums2[index_n]:
-                nums1[ptr] = nums1[index_m]
-                index_m -= 1 
+        while j >= 0:
+            if i >= 0 and nums1[i] >= nums2[j]:
+                nums1[last] = nums1[i]
+                i -= 1
             else:
-                nums1[ptr] = nums2[index_n]
-                index_n -= 1
-            ptr -= 1
+                nums1[last] = nums2[j]
+                j -= 1
+            last -= 1
