9aa9f7346638960a7e24577e274f3855b6226d37
Kushal Patel <61028285+kushalpatel2210@users.noreply.github.com>
2025-04-05 19:06:09 -0400

    Time: 0 ms (100%), Space: 17.8 MB (49.01%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,15 +1,18 @@
-# DP - Bottom-up: memory optimized
+# DP - Top-up: Memoization
 class Solution:
     def climbStairs(self, n: int) -> int:
-        if n <= 2:
-            return n
+        memo = { 0 : 1 }
 
-        prev, curr = 1, 2
+        def steps(currStep):
+            if currStep < 0:
+                return 0
+            elif currStep in memo:
+                return memo[currStep]
+            else:
+                memo[currStep] = steps(currStep - 1) + steps(currStep - 2)
+                return memo[currStep]
 
-        for i in range(n - 2):
-            prev, curr = curr, prev + curr
-        
-        return curr
+        return steps(n)
 
 '''
 # DP - Top-up: Memoization
@@ -41,4 +44,17 @@ class Solution:
             dp[i] = dp[i - 1] + dp[i - 2]
         
         return dp[n]
+
+# DP - Bottom-up: memory optimized
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        if n <= 2:
+            return n
+
+        prev, curr = 1, 2
+
+        for i in range(n - 2):
+            prev, curr = curr, prev + curr
+        
+        return curr
 '''
