a0d8e5a8a42fe293edff5e17c0cb47ec4cf58a8c
Kushal Patel <61028285+kushalpatel2210@users.noreply.github.com>
2025-04-05 18:19:51 -0400

    Time: 39 ms (64.49%), Space: 17.8 MB (24.44%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.py
+++ 1013-fibonacci-number/1013-fibonacci-number.py
@@ -1,3 +1,20 @@
+# Top Down DP
+class Solution:
+    def fib(self, n: int) -> int:
+        memo = {0 : 0, 1 : 1}
+
+        def f(x):
+            if x in memo:
+                return memo[x]
+            else:
+                memo[x] = f(x - 1) + f(x - 2)
+                return memo[x]
+        
+        return f(n)
+        
+
+'''
+# Noraml Solution
 class Solution:
     def fib(self, n: int) -> int:
         
@@ -5,3 +22,4 @@ class Solution:
             return n
         
         return self.fib(n - 1) + self.fib(n - 2)
+'''
