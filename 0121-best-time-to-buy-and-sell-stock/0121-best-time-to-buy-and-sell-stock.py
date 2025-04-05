352b75372f072a1ce3ee8d7f5d33e4af045a139d
Kushal Patel <61028285+kushalpatel2210@users.noreply.github.com>
2025-04-05 13:49:41 -0400

    Time: 94 ms (54.01%), Space: 26.9 MB (33.16%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -1,5 +1,20 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
+        l, r = 0, 1
+        maximumProfit = 0
+
+        while r < len(prices):
+            if prices[l] < prices[r]:
+                maximumProfit = max(maximumProfit, prices[r] - prices[l])
+            else:
+                l = r
+            r += 1
+        
+        return maximumProfit
+
+'''
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
         maximumProfit = 0
         buy = float('inf')
 
@@ -10,3 +25,4 @@ class Solution:
                 maximumProfit = max(maximumProfit, price - buy)
             
         return maximumProfit
+'''
