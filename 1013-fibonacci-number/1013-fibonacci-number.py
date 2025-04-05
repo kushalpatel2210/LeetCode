9d599ed3c9ec7dcdc0e7ff1f5c410189e7d30d0f
Kushal Patel <61028285+kushalpatel2210@users.noreply.github.com>
2025-04-05 18:29:24 -0400

    Time: 34 ms (85.64%), Space: 17.8 MB (24.44%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.py
+++ 1013-fibonacci-number/1013-fibonacci-number.py
@@ -1,4 +1,30 @@
-# Top Down DP
+# Bottom-up - Tabulation
+class Solution:
+    def fib(self, n: int) -> int:
+        if n == 0 or n == 1:
+            return n
+        
+        dp = [0] * (n + 1)
+        dp[0] = 0
+        dp[1] = 1
+
+        for i in range(2, n + 1):
+            dp[i] = dp[i - 1] + dp[i - 2]
+        
+        return dp[n]
+        
+
+'''
+# Noraml Solution
+class Solution:
+    def fib(self, n: int) -> int:
+        
+        if n == 0 or n == 1:
+            return n
+        
+        return self.fib(n - 1) + self.fib(n - 2)
+
+# Top Down DP - Memoization
 class Solution:
     def fib(self, n: int) -> int:
         memo = {0 : 0, 1 : 1}
@@ -12,14 +38,4 @@ class Solution:
         
         return f(n)
         
-
-'''
-# Noraml Solution
-class Solution:
-    def fib(self, n: int) -> int:
-        
-        if n == 0 or n == 1:
-            return n
-        
-        return self.fib(n - 1) + self.fib(n - 2)
 '''
