ce1f785b4f5bd0e750d70a624f8a90e28254267f
Kushal Patel <61028285+kushalpatel2210@users.noreply.github.com>
2025-04-06 18:24:22 -0400

    Time: 164 ms (31.1%), Space: 78.6 MB (27.53%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.py
+++ 0146-lru-cache/0146-lru-cache.py
@@ -1,23 +1,22 @@
-class ListNode: # Doubly linked list
-
+class ListNode:
     def __init__(self, key=0, val=0):
-        self.key, self.val = key, val
+        self.key = key
+        self.val = val
         self.next = self.prev = None
-
 
 class LRUCache:
 
     def __init__(self, capacity: int):
+        self.cache = dict()
         self.cap = capacity
-        self.cache = dict() # key, ref to listnode
         self.start, self.end = ListNode(), ListNode()
-        self.start.next, self.end.prev = self.end, self.start # Connect start and end
+        self.start.next, self.end.prev = self.end, self.start
     
     def remove(self, node):
         prev, nxt = node.prev, node.next
         prev.next, nxt.prev = nxt, prev
     
-    def insert(self, node): # insert it at the end 
+    def insert(self, node):
         prev, nxt = self.end.prev, self.end
         prev.next = nxt.prev = node
         node.next, node.prev = nxt, prev
@@ -25,24 +24,22 @@ class LRUCache:
     def get(self, key: int) -> int:
         if key in self.cache:
             node = self.cache[key]
-            self.remove(node) # remove
+            self.remove(node) # remove 
             self.insert(node) # insert
             return node.val
         return -1
-        
 
     def put(self, key: int, value: int) -> None:
         if key in self.cache:
             self.remove(self.cache[key]) # remove
         self.cache[key] = ListNode(key, value)
-        self.insert(self.cache[key]) # insert
+        self.insert(self.cache[key])
 
+        # cap
         if len(self.cache) > self.cap:
             lru = self.start.next
             self.remove(lru)
             del self.cache[lru.key]
-                    
-
 
 # Your LRUCache object will be instantiated and called as such:
 # obj = LRUCache(capacity)
