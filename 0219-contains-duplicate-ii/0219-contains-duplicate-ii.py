102f85d0cff1283bd7dc446419866c17374f58ed
Kushal Patel <61028285+kushalpatel2210@users.noreply.github.com>
2025-04-05 21:51:14 -0400

    Time: 23 ms (58%), Space: 36.6 MB (11.9%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.py
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.py
@@ -1,44 +1,12 @@
 class Solution:
     def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
-        hashMap = {}
+        hashMap = dict()
 
-        for i in range(len(nums)):
-            if nums[i] in hashMap and i - hashMap[nums[i]] <= k:
-                return True
-            hashMap[nums[i]] = i
-
-        return False
-
-'''
-class Solution:
-    def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
-        L = 0 
-        window = set()
-
-        for R in range(len(nums)):
-            if R - L > k:
-                window.remove(nums[L])
-                L += 1
-            if nums[R] in window:
-                return True
-            window.add(nums[R])
-        
-        return False
-'''
-
-'''
-class Solution:
-    def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
-        numbers = set()
-        
-        for i in range(len(nums)):
-            if nums[i] in numbers:
-                return True
+        for i, num in enumerate(nums):
+            if num in hashMap:
+                if abs(hashMap[num] - i) <= k:
+                    return True
             
-            numbers.add(nums[i])
-            
-            if len(numbers) > k:
-                numbers.remove(nums[i - k])
+            hashMap[num] = i
         
         return False
-'''
