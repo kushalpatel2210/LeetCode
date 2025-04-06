b0f2dca1ff6523684890261903b8925fcf121b12
Kushal Patel <61028285+kushalpatel2210@users.noreply.github.com>
2025-04-06 09:11:10 -0400

    Time: 19 ms (37.48%), Space: 22.8 MB (5.45%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -2,6 +2,21 @@ from collections import defaultdict
 
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        res = defaultdict(list)
+
+        for word in strs:
+            count = [0] * 26
+            for c in word:
+                count[ord(c) - ord('a')] += 1
+            res[tuple(count)].append(word)
+        
+        return list(res.values())
+
+'''
+from collections import defaultdict
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         hashMap = defaultdict(list)
 
         for word in strs:
@@ -9,3 +24,4 @@ class Solution:
             hashMap[sortedWord].append(word)
         
         return list(hashMap.values())
+'''
