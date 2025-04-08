5bbdaf1e14a2b7347543d138acd562e6f241d699
Kushal Patel <61028285+kushalpatel2210@users.noreply.github.com>
2025-04-08 06:36:19 -0400

    Time: 1413 ms (13.34%), Space: 21.8 MB (11.47%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,21 +1,52 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
+        res = set()
         nums.sort()
-        triplets = set()
 
         for i, num in enumerate(nums):
             l, r = i + 1, len(nums) - 1
-
+            
             while l < r:
-                diff = nums[l] + nums[r] + num
+                currSum = num + nums[l] + nums[r]
 
-                if diff == 0:
-                    triplets.add((num, nums[l], nums[r]))
+                if currSum == 0:
+                    res.add((num, nums[l], nums[r]))
                     l += 1
                     r -= 1
-                elif diff > 0:
+                elif currSum > 0:
                     r -= 1
                 else:
                     l += 1
-         
-        return [list(tpl) for tpl in triplets]
+        
+        return [list(ele) for ele in res]
+
+'''
+# backtracking but inefficient
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        res = list()
+        nums.sort()
+
+        def backtrack(index, currPath, total):
+            if len(currPath) == 3 and total == 0:
+                res.append(currPath.copy())
+                return
+
+            if index >= len(nums):
+                return
+
+            currElement = nums[index]
+            currPath.append(currElement) #append
+            backtrack(index + 1, currPath, total + currElement) #explore
+            currPath.pop()
+            
+
+            while index < len(nums) and nums[index] == currElement:
+                index += 1
+
+            backtrack(index, currPath, total)
+
+        backtrack(0, [], 0)
+
+        return res
+'''
