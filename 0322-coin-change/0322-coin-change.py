7f57a7c16e63293efef27879df8bd0433cc64ab9
Kushal Patel <61028285+kushalpatel2210@users.noreply.github.com>
2025-04-13 14:34:11 -0400

    Time: 739 ms (67.89%), Space: 18 MB (87.84%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -1,3 +1,19 @@
+# Bottom up 
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        dp = [amount + 1] * (amount + 1)
+        dp[0] = 0
+
+        for a in range(amount + 1):
+            for c in coins:
+                if a - c >= 0:
+                    dp[a] = min(dp[a], 1 + dp[a - c])
+        
+        return dp[amount] if dp[amount] != amount + 1 else -1
+
+
+'''
+# Top Down
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
         memo = {}
@@ -19,19 +35,4 @@ class Solution:
         minCoins = dfs(amount)
 
         return minCoins if minCoins != 1e9 else -1
-
-'''
-# Bottom up 
-class Solution:
-    def coinChange(self, coins: List[int], amount: int) -> int:
-        dp = [amount + 1] * (amount + 1)
-        dp[0] = 0
-
-        for a in range(amount + 1):
-            for c in coins:
-                if a - c >= 0:
-                    dp[a] = min(dp[a], 1 + dp[a - c])
-        
-        return dp[amount] if dp[amount] != amount + 1 else -1
-
 '''
