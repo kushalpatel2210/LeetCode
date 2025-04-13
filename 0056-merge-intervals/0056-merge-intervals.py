bea38d03e4c19251c449615605affbc1961cb99e
Kushal Patel <61028285+kushalpatel2210@users.noreply.github.com>
2025-04-13 18:00:40 -0400

    Time: 19 ms (5.34%), Space: 20.9 MB (63.27%) - LeetHub

---
+++ 0056-merge-intervals/0056-merge-intervals.py
@@ -0,0 +1,19 @@
+class Solution:
+    def isOverlapping(self, interval1, interval2):
+        start = max(interval1[0], interval2[0])
+        end = min(interval1[1], interval2[1])
+        return end - start >= 0
+
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        intervals.sort()
+        res = [intervals[0]]
+
+        for interval in intervals:
+            currInterval = res[-1]
+
+            if self.isOverlapping(currInterval, interval):
+                res[-1] = [min(interval[0], currInterval[0]), max(interval[1], currInterval[1])]
+            else:
+                res.append(interval)
+
+        return res
