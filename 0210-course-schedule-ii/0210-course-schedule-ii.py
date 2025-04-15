7657c639978695c99f7a96d2717e59a5d38717f5
Kushal Patel <61028285+kushalpatel2210@users.noreply.github.com>
2025-04-15 10:32:33 -0400

    Time: 7 ms (28.37%), Space: 19.9 MB (5.96%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.py
+++ 0210-course-schedule-ii/0210-course-schedule-ii.py
@@ -1,3 +1,42 @@
+# DFS
+
+class Solution:
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        adj = { i: [] for i in range(numCourses) }
+        visitSet, cycle = set(), set()
+        res = []
+
+        for crs, pre in prerequisites:
+            adj[crs].append(pre)
+
+        def dfs(crs):
+            if crs in cycle:
+                return False
+            
+            if crs in visitSet:
+                return True
+
+            cycle.add(crs)
+            visitSet.add(crs)
+
+            for nei in adj[crs]:
+                if not dfs(nei):
+                    return False
+
+            res.append(crs)
+            cycle.remove(crs)
+            return True
+
+        for i in range(numCourses):
+            if not dfs(i):
+                return []
+        
+        return res
+        
+
+
+
+'''
 # DAG - Topological Sort
 from collections import deque
 
@@ -28,4 +67,6 @@ class Solution:
                     q.append(nei)
 
         return [] if processed != numCourses else res[::-1]
+        
+'''
         
