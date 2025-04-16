925b23ce7b314a36ed7929d26133732ba3ab6899
Kushal Patel <61028285+kushalpatel2210@users.noreply.github.com>
2025-04-16 11:11:39 -0400

    Time: 1 ms (19.37%), Space: 17.9 MB (12.29%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -7,9 +7,9 @@ class Solution:
         i = 0
 
         while i < len(nums) and ptrVal < 3:
-            print(f"i {i} nums[i] {nums[i]} ptrVal {ptrVal}")
             currVal = nums[i]
             j = i + 1
+            
             if nums[i] == ptrVal:
                 i += 1
             
@@ -17,15 +17,10 @@ class Solution:
                 j += 1
             
             if j < len(nums) and nums[j] == ptrVal:
-                print(f"j {j} nums[j] {nums[j]}")
                 nums[i], nums[j] = nums[j], nums[i]
             else:
-                print(f"j {j}")
                 ptrVal += 1
                 continue
-            print(f"ptrVal {ptrVal}")
-            print(f"nums {nums}")
-            print("-------------")
             i += 1
         
 
