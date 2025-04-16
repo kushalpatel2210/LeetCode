e6f1834fb7733cb11dd1e194daf81843e12aa480
Kushal Patel <61028285+kushalpatel2210@users.noreply.github.com>
2025-04-16 11:15:16 -0400

    Time: 0 ms (100%), Space: 17.6 MB (83.32%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -3,6 +3,25 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
+        # Counting sort
+        count = [0] * 3
+        for num in nums:
+            count[num] += 1
+        
+        index = 0
+        for i in range(3):
+            while count[i]:
+                nums[index] = i
+                count[i] -= 1
+                index += 1
+
+
+'''
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
         ptrVal = 0
         i = 0
 
@@ -22,6 +41,4 @@ class Solution:
                 ptrVal += 1
                 continue
             i += 1
-        
-
-            
+'''
