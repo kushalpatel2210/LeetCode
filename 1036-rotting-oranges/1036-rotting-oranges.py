ad91686c3011db876270de4ea1ebc306876bceb8
Kushal Patel <61028285+kushalpatel2210@users.noreply.github.com>
2025-04-16 08:39:32 -0400

    Time: 7 ms (27.22%), Space: 17.7 MB (93.56%) - LeetHub

--- 1036-rotting-oranges/1036-rotting-oranges.py
+++ 1036-rotting-oranges/1036-rotting-oranges.py
@@ -1,33 +1,37 @@
 from collections import deque
-
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
-        rows, cols = len(grid), len(grid[0])
-        queue = deque([])
+        freshOranges = 0
+        m, n = len(grid), len(grid[0])
+        q = deque()
         minutes = 0
-        fresh_oranges = 0 
 
-        for i in range(len(grid)):
-            for j in range(len(grid[i])):
-                if grid[i][j] == 2:
-                    queue.append((i, j))
-                elif grid[i][j] == 1:
-                    fresh_oranges += 1
+        for i in range(m):
+            for j in range(n):
+                orange = grid[i][j]
+                if orange == 1:
+                    freshOranges += 1
+                elif orange == 2:
+                    q.append((i, j))
         
-        if fresh_oranges == 0:
+        if freshOranges == 0:
             return 0
-        
-        while queue:
-            minutes += 1
-            for _ in range(len(queue)):
-                row, col = queue.popleft()
-                positions = [[0, -1], [0, 1], [-1, 0], [1, 0]]
 
-                for direct_row, direct_col in positions:
-                    new_row, new_col = row + direct_row, col + direct_col
-                    if 0 <= new_row < rows and 0 <= new_col < cols and grid[new_row][new_col] == 1:
-                        queue.append((new_row, new_col))
-                        grid[new_row][new_col] = 2
-                        fresh_oranges -= 1
+        while q:
+            newRotten = False
 
-        return minutes - 1 if fresh_oranges == 0 else -1
+            for _ in range(len(q)):
+                i, j = q.popleft()
+
+                for deltaI, deltaJ in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
+                    ni, nj = i + deltaI, j + deltaJ
+                    if 0 <= ni < m and 0 <= nj < n and grid[ni][nj] == 1:
+                        q.append((ni, nj))
+                        grid[ni][nj] = 2
+                        freshOranges -= 1
+                        newRotten = True
+            
+            if newRotten:
+                minutes += 1
+                        
+        return minutes if freshOranges == 0 else -1
