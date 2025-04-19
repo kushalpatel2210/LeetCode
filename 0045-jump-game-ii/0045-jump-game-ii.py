6c2c427d1829abb2d0d9c81fbdf9c531071075ca
Kushal Patel <61028285+kushalpatel2210@users.noreply.github.com>
2025-04-19 10:50:11 -0400

    Time: 7 ms (79.19%), Space: 19 MB (11.22%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.py
+++ 0045-jump-game-ii/0045-jump-game-ii.py
@@ -1,5 +1,22 @@
 class Solution:
     def jump(self, nums: List[int]) -> int:
+        res = 0
+        l = r = 0
+
+        while r < len(nums) - 1:
+            farthest = 0
+            for i in range(l, r + 1):
+                farthest = max(farthest, i + nums[i])
+            l = r + 1
+            r = farthest
+            res += 1
+        
+        return res
+
+'''
+DP: Top Down 
+class Solution:
+    def jump(self, nums: List[int]) -> int:
         memo = {}
 
         def dfs(i):
@@ -19,3 +36,4 @@ class Solution:
             return minJump
 
         return dfs(0)
+'''
