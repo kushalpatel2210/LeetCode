79c907ea939749896ad0f4b4ab1e347200ca5461
Kushal Patel <61028285+kushalpatel2210@users.noreply.github.com>
2025-04-20 17:03:34 -0400

    Time: 0 ms (100%), Space: 17.7 MB (54.71%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.py
@@ -0,0 +1,13 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        memo = [-1] * len(nums)
+
+        def dfs(i):
+            if i >= len(nums):
+                return 0
+            if memo[i] != -1:
+                return memo[i]
+            memo[i] = max(dfs(i + 1), nums[i] + dfs(i + 2))
+            return memo[i]
+        
+        return dfs(0)
