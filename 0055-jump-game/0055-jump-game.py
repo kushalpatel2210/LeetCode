db2d35c8e1112e68ccc0ab65473e1dfd5cd0e981
Kushal Patel <61028285+kushalpatel2210@users.noreply.github.com>
2025-04-29 21:25:44 -0400

    Time: 10 ms (91.79%), Space: 18.7 MB (25.54%) - LeetHub

--- 0055-jump-game/0055-jump-game.py
+++ 0055-jump-game/0055-jump-game.py
@@ -2,33 +2,8 @@ class Solution:
     def canJump(self, nums: List[int]) -> bool:
         goal = len(nums) - 1
 
-        for i in range(len(nums) -2, -1, -1):
+        for i in range(len(nums) - 2, -1, -1):
             if i + nums[i] >= goal:
-                goal = i 
-        
+                goal = i
+
         return goal == 0
-
-'''
-# DP: Top Down 
-class Solution:
-    def canJump(self, nums: List[int]) -> bool:
-        memo = dict()
-
-        def backtrack(i):
-            if i in memo:
-                return memo[i]
-            if i == len(nums) - 1:
-                return True
-            if nums[i] == 0:
-                return False
-            
-            end = min(len(nums), i + nums[i] + 1)
-            for j in range(i + 1, end):
-                if backtrack(j):
-                    memo[i] = True
-                    return True
-            memo[i] = False
-            return False
-        
-        return backtrack(0)
-'''
