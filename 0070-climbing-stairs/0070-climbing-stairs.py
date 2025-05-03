b0757f46473e6e98efc05817a053d304502d7301
Kushal Patel <61028285+kushalpatel2210@users.noreply.github.com>
2025-05-03 18:30:16 -0400

    Time: 0 ms (100%), Space: 17.8 MB (48.64%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,60 +1,16 @@
-# DP - Top-up: Memoization
 class Solution:
     def climbStairs(self, n: int) -> int:
-        memo = { 0 : 1 }
+        memo = {}
 
-        def steps(currStep):
-            if currStep < 0:
-                return 0
-            elif currStep in memo:
-                return memo[currStep]
-            else:
-                memo[currStep] = steps(currStep - 1) + steps(currStep - 2)
-                return memo[currStep]
+        def dp(step):
+            if step <= 2:
+                return step
+            
+            if step in memo:
+                return memo[step]
+            
+            memo[step] = dp(step - 1) + dp(step - 2)
 
-        return steps(n)
-
-'''
-# DP - Top-up: Memoization
-class Solution:
-    def climbStairs(self, n: int) -> int:
-        memo = { 0 : 1 }
-
-        def steps(currStep):
-            if currStep < 0:
-                return 0
-            elif currStep in memo:
-                return memo[currStep]
-            else:
-                memo[currStep] = steps(currStep - 1) + steps(currStep - 2)
-                return memo[currStep]
-
-        return steps(n)
-
-# DP - Bottom-up: Tabulation
-class Solution:
-    def climbStairs(self, n: int) -> int:
-        if n <= 2:
-            return n
-
-        dp = [0] * (n + 1)
-        dp[1], dp[2] = 1, 2
-
-        for i in range(3, n + 1):
-            dp[i] = dp[i - 1] + dp[i - 2]
+            return memo[step]
         
-        return dp[n]
-
-# DP - Bottom-up: memory optimized
-class Solution:
-    def climbStairs(self, n: int) -> int:
-        if n <= 2:
-            return n
-
-        prev, curr = 1, 2
-
-        for i in range(n - 2):
-            prev, curr = curr, prev + curr
-        
-        return curr
-'''
+        return dp(n)
