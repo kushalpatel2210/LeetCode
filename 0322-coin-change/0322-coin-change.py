7ebb3bb012ec03764edfa1ffd17610ad9ad02a5c
Kushal Patel <61028285+kushalpatel2210@users.noreply.github.com>
2025-05-03 18:48:03 -0400

    Time: 1095 ms (21.07%), Space: 45.9 MB (6.75%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -1,38 +1,20 @@
-# Bottom up 
-class Solution:
-    def coinChange(self, coins: List[int], amount: int) -> int:
-        dp = [amount + 1] * (amount + 1)
-        dp[0] = 0
-
-        for a in range(amount + 1):
-            for c in coins:
-                if a - c >= 0:
-                    dp[a] = min(dp[a], 1 + dp[a - c])
-        
-        return dp[amount] if dp[amount] != amount + 1 else -1
-
-
-'''
-# Top Down
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
         memo = {}
 
-        def dfs(amount):
-            if amount == 0:
+        def dp(target):
+            if target == 0:
                 return 0
-            if amount in memo:
-                return memo[amount]
             
-            res = 1e9
+            if target in memo:
+                return memo[target]
+            
+            minCoins = float('inf')
             for coin in coins:
-                if amount - coin >= 0:
-                    res = min(res, 1 + dfs(amount - coin))
-            
-            memo[amount] = res
-            return res
-        
-        minCoins = dfs(amount)
+                if coin <= target:
+                    minCoins = min(minCoins, 1 + dp(target - coin))
+            memo[target] = minCoins
 
-        return minCoins if minCoins != 1e9 else -1
-'''
+            return memo[target]
+
+        return dp(amount) if dp(amount) != float('inf') else -1
