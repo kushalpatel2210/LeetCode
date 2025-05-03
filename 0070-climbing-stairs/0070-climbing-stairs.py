bc74a131ca19fddc7bb41eff7b5b660e50fc514b
Kushal Patel <61028285+kushalpatel2210@users.noreply.github.com>
2025-05-03 18:35:09 -0400

    Time: 0 ms (100%), Space: 17.8 MB (30.32%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,3 +1,19 @@
+# Bottom-up DP
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        if n <= 2:
+            return n
+            
+        dp = [0] * (n + 1)
+        dp[1], dp[2] = 1, 2
+
+        for i in range(3, n + 1):
+            dp[i] = dp[i - 1] + dp[i - 2]
+        
+        return dp[n]
+
+'''
+# Top Down DP
 class Solution:
     def climbStairs(self, n: int) -> int:
         memo = {}
@@ -7,10 +23,9 @@ class Solution:
                 return step
             
             if step in memo:
-                return memo[step]
+                 return memo[step]
             
             memo[step] = dp(step - 1) + dp(step - 2)
 
             return memo[step]
-        
-        return dp(n)
+'''
