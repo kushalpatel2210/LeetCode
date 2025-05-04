5a8ff6be57b09297d76515495acc1d6db869a01b
Kushal Patel <61028285+kushalpatel2210@users.noreply.github.com>
2025-05-04 12:51:23 -0400

    Time: 0 ms (100%), Space: 17.8 MB (34.45%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -1,10 +1,17 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        rob1, rob2 = 0, 0
+        n = len(nums)
+        if n == 1:
+            return nums[0]
 
-        for num in nums:
-            tmp = max(num + rob1, rob2)
-            rob1 = rob2
-            rob2 = tmp
+        dp = [0] * len(nums)
+        dp[0] = nums[0]
+        dp[1] = max(nums[0], nums[1])
+
+        for i in range(2, n):
+            # Two choises 
+            # 1. Either you skip the current house
+            # 2. If you choose to rob current house then you need to rob prev->prev house
+            dp[i] = max(dp[i - 1], nums[i] + dp[i - 2])
         
-        return rob2
+        return dp[n - 1]
