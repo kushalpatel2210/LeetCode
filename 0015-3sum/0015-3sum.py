1aab45d29eb8705a334063f58161b593417c7912
Kushal Patel <61028285+kushalpatel2210@users.noreply.github.com>
2025-05-04 20:06:00 -0400

    Time: 1496 ms (11.17%), Space: 21.9 MB (7.57%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,60 +1,21 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        res = list()
         nums.sort()
+        res = set()
 
-        for i, num in enumerate(nums):
-            if num > 0: # because nums is sorted
-                break
+        for i in range(len(nums)):
+            left, right = i + 1, len(nums) - 1
 
-            if i > 0 and num == nums[i - 1]:
-                continue
-                            
-            l, r = i + 1, len(nums) - 1
-            
-            while l < r:
-                currSum = num + nums[l] + nums[r]
+            while left < right:
+                currSum = nums[i] + nums[left] + nums[right]
 
                 if currSum == 0:
-                    res.append([num, nums[l], nums[r]])
-                    l += 1
-                    r -= 1
-                    while l < r and nums[l] == nums[l - 1]:
-                        l += 1
+                    res.add((nums[i], nums[left], nums[right]))
+                    left += 1
+                    right -= 1
                 elif currSum > 0:
-                    r -= 1
+                    right -= 1
                 else:
-                    l += 1
+                    left += 1
         
-        return res
-
-'''
-# backtracking but inefficient
-class Solution:
-    def threeSum(self, nums: List[int]) -> List[List[int]]:
-        res = list()
-        nums.sort()
-
-        def backtrack(index, currPath, total):
-            if len(currPath) == 3 and total == 0:
-                res.append(currPath.copy())
-                return
-
-            if index >= len(nums):
-                return
-
-            currElement = nums[index]
-            currPath.append(currElement) #append
-            backtrack(index + 1, currPath, total + currElement) #explore
-            currPath.pop()
-            
-
-            while index < len(nums) and nums[index] == currElement:
-                index += 1
-
-            backtrack(index, currPath, total)
-
-        backtrack(0, [], 0)
-
-        return res
-'''
+        return [list(triplet) for triplet in res]
