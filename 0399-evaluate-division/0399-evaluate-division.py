c652d5faf255c1d484a09d46ca1e04830be26cc7
Kushal Patel <61028285+kushalpatel2210@users.noreply.github.com>
2025-05-05 19:58:00 -0400

    Time: 0 ms (100%), Space: 17.7 MB (93.94%) - LeetHub

--- 0399-evaluate-division/0399-evaluate-division.py
+++ 0399-evaluate-division/0399-evaluate-division.py
@@ -1,28 +1,29 @@
+from collections import defaultdict, deque
+
 class Solution:
     def calcEquation(self, equations: List[List[str]], values: List[float], queries: List[List[str]]) -> List[float]:
         adj = defaultdict(list)
-
-        # directed graph
+        
         for i in range(len(equations)):
-            adj[equations[i][0]].append((equations[i][1], values[i])) # dest, weight
+            adj[equations[i][0]].append((equations[i][1], values[i]))
             adj[equations[i][1]].append((equations[i][0], 1 / values[i]))
         
-        def bfs(src, dest):
-            if src not in adj or dest not in adj:
+        def dfs(src, dst):
+            if src not in adj or dst not in adj:
                 return -1
             
             q = deque([(src, 1)])
             visited = set()
 
             while q:
-                node, w = q.popleft()
-                if node == dest:
-                    return w
+                source, weight = q.popleft()
+                if source == dst:
+                    return weight
                 
-                for nei, weight in adj[node]:
+                for nei, wei in adj[source]:
                     if nei not in visited:
-                        q.append((nei, w * weight))
+                        q.append((nei, weight * wei))
                         visited.add(nei)
             return -1 
-
-        return [bfs(q[0], q[1]) for q in queries] 
+        
+        return [dfs(q[0], q[1]) for q in queries]
