afef37d7e993bb092ce7f143705b301da3455194
Kushal Patel <61028285+kushalpatel2210@users.noreply.github.com>
2025-05-08 20:49:11 -0400

    Time: 96 ms (72.7%), Space: 28.5 MB (67.8%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.py
+++ 0011-container-with-most-water/0011-container-with-most-water.py
@@ -9,7 +9,10 @@ class Solution:
 
             if height[l] < height[r]:
                 l += 1
-            else:
+            elif height[r] < height[l]:
+                r -= 1
+            else: # Both are equal
+                l += 1
                 r -= 1
         
         return maxWater
