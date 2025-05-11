f7f1577d5ff7be10dd18da4e882bfa6b4763f6b7
Kushal Patel <61028285+kushalpatel2210@users.noreply.github.com>
2025-05-10 22:33:49 -0400

    Time: 0 ms (100%), Space: 17.7 MB (54.77%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -3,15 +3,12 @@ class Solution:
         n = len(nums)
         if n == 1:
             return nums[0]
-
-        dp = [0] * len(nums)
+        
+        dp = [0] * n
         dp[0] = nums[0]
         dp[1] = max(nums[0], nums[1])
 
         for i in range(2, n):
-            # Two choises 
-            # 1. Either you skip the current house
-            # 2. If you choose to rob current house then you need to rob prev->prev house
-            dp[i] = max(dp[i - 1], nums[i] + dp[i - 2])
-        
+            dp[i] = max(nums[i] + dp[i - 2], dp[i - 1])
+
         return dp[n - 1]
