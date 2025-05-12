deb1d2c99b47b420b9cdcebd77badb65db3131f6
Kushal Patel <61028285+kushalpatel2210@users.noreply.github.com>
2025-05-11 21:48:13 -0400

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1036-rotting-oranges/1036-rotting-oranges.py
+++ 1036-rotting-oranges/1036-rotting-oranges.py
@@ -1,37 +1,34 @@
 from collections import deque
+
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
-        freshOranges = 0
         m, n = len(grid), len(grid[0])
+        freshOranges = 0
         q = deque()
         minutes = 0
 
         for i in range(m):
             for j in range(n):
-                orange = grid[i][j]
-                if orange == 1:
+                if grid[i][j] == 1:
                     freshOranges += 1
-                elif orange == 2:
+                elif grid[i][j] == 2:
                     q.append((i, j))
         
         if freshOranges == 0:
             return 0
 
         while q:
-            newRotten = False
+            i, j = q.popleft()
+            rottenOrange = False
 
-            for _ in range(len(q)):
-                i, j = q.popleft()
-
-                for deltaI, deltaJ in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
-                    ni, nj = i + deltaI, j + deltaJ
-                    if 0 <= ni < m and 0 <= nj < n and grid[ni][nj] == 1:
-                        q.append((ni, nj))
-                        grid[ni][nj] = 2
-                        freshOranges -= 1
-                        newRotten = True
+            for deltaI, deltaJ in [(0, 1), (0, -1), (-1, 0), (1, 0)]:
+                if 0 <= i + deltaI < m and 0 <= j + deltaJ < n and grid[i + deltaI][j + deltaJ] == 1:
+                    q.append((i + deltaI, j + deltaJ))
+                    freshOranges -= 1
+                    grid[i + deltaI][j + deltaJ] = 2
+                    rottenOrange = True
             
-            if newRotten:
+            if rottenOrange:
                 minutes += 1
-                        
+
         return minutes if freshOranges == 0 else -1
