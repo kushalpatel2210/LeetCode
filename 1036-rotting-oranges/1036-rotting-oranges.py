6fcc4f5678a4878fe82b3c3d3063a337cde034ae
Kushal Patel <61028285+kushalpatel2210@users.noreply.github.com>
2025-05-11 21:49:53 -0400

    Time: 3 ms (82.36%), Space: 17.8 MB (71.54%) - LeetHub

--- 1036-rotting-oranges/1036-rotting-oranges.py
+++ 1036-rotting-oranges/1036-rotting-oranges.py
@@ -18,16 +18,18 @@ class Solution:
             return 0
 
         while q:
-            i, j = q.popleft()
             rottenOrange = False
 
-            for deltaI, deltaJ in [(0, 1), (0, -1), (-1, 0), (1, 0)]:
-                if 0 <= i + deltaI < m and 0 <= j + deltaJ < n and grid[i + deltaI][j + deltaJ] == 1:
-                    q.append((i + deltaI, j + deltaJ))
-                    freshOranges -= 1
-                    grid[i + deltaI][j + deltaJ] = 2
-                    rottenOrange = True
-            
+            for _ in range(len(q)):
+                i, j = q.popleft()
+
+                for deltaI, deltaJ in [(0, 1), (0, -1), (-1, 0), (1, 0)]:
+                    if 0 <= i + deltaI < m and 0 <= j + deltaJ < n and grid[i + deltaI][j + deltaJ] == 1:
+                        q.append((i + deltaI, j + deltaJ))
+                        freshOranges -= 1
+                        grid[i + deltaI][j + deltaJ] = 2
+                        rottenOrange = True
+                
             if rottenOrange:
                 minutes += 1
 
