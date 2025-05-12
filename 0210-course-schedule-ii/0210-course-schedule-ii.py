4fce8d9cd445855268430e99f2f5dc7ae61a7968
Kushal Patel <61028285+kushalpatel2210@users.noreply.github.com>
2025-05-11 22:26:11 -0400

    Time: 0 ms (100%), Space: 19.2 MB (47.66%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.py
+++ 0210-course-schedule-ii/0210-course-schedule-ii.py
@@ -1,72 +1,30 @@
-# DFS
+from collections import defaultdict, deque
 
 class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
-        adj = { i: [] for i in range(numCourses) }
-        visitSet, cycle = set(), set()
-        res = []
-
-        for crs, pre in prerequisites:
-            adj[crs].append(pre)
-
-        def dfs(crs):
-            if crs in cycle:
-                return False
-            
-            if crs in visitSet:
-                return True
-
-            cycle.add(crs)
-            visitSet.add(crs)
-
-            for nei in adj[crs]:
-                if not dfs(nei):
-                    return False
-
-            res.append(crs)
-            cycle.remove(crs)
-            return True
-
-        for i in range(numCourses):
-            if not dfs(i):
-                return []
-        
-        return res
-        
-
-
-
-'''
-# DAG - Topological Sort
-from collections import deque
-
-class Solution:
-    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
-        adj = { i: [] for i in range(numCourses) }
-        indegree = [0] * numCourses
-        res = []
-
-        for crs, pre in prerequisites:
-            adj[crs].append(pre)
-            indegree[pre] += 1
-        
+        preMap = defaultdict(list)
         q = deque()
+        inDegree = [0] * numCourses 
+        seq = list()
+
+        for crs, preq in prerequisites:
+            preMap[crs].append(preq)
+            inDegree[preq] += 1
+        
         for i in range(numCourses):
-            if indegree[i] == 0:
+            if inDegree[i] == 0:
                 q.append(i)
         
         processed = 0
         while q:
             node = q.popleft()
-            res.append(node)
+            seq.append(node)
             processed += 1
 
-            for nei in adj[node]:
-                indegree[nei] -= 1
-                if indegree[nei] == 0:
-                    q.append(nei)
+            for preq in preMap[node]:
+                inDegree[preq] -= 1
 
-        return [] if processed != numCourses else res[::-1]
+                if inDegree[preq] == 0:
+                    q.append(preq)
         
-'''
-        
+        return [] if processed != numCourses else seq[::-1]
