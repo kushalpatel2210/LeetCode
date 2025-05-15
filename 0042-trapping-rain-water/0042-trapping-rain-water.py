b2495585f69d3d5d9e78404fed36b394068ef908
Kushal Patel <61028285+kushalpatel2210@users.noreply.github.com>
2025-05-15 08:10:04 -0400

    Time: 14 ms (57.07%), Space: 19.3 MB (69.67%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.py
+++ 0042-trapping-rain-water/0042-trapping-rain-water.py
@@ -1,3 +1,27 @@
+# Space Optimized
+class Solution:
+    def trap(self, height: List[int]) -> int:
+
+        if not height:
+            return 0
+
+        l, r = 0, len(height) - 1
+        maxLeft, maxRight = height[l], height[r]
+        res = 0
+
+        while l < r:
+            if maxLeft < maxRight:
+                l += 1
+                maxLeft = max(maxLeft, height[l])
+                res += maxLeft - height[l]
+            else:
+                r -= 1
+                maxRight = max(maxRight, height[r])
+                res += maxRight - height[r]
+        
+        return res
+
+"""
 class Solution:
     def trap(self, height: List[int]) -> int:
         n = len(height)
@@ -18,3 +42,4 @@ class Solution:
             trappedWater += containerSize - height[i]
 
         return trappedWater
+"""
