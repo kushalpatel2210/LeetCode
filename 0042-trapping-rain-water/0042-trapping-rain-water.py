780adb765d89fc20ec1d4f71d024105604570927
Kushal Patel <61028285+kushalpatel2210@users.noreply.github.com>
2025-05-15 07:41:25 -0400

    Time: 27 ms (30.11%), Space: 19.4 MB (19.62%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.py
+++ 0042-trapping-rain-water/0042-trapping-rain-water.py
@@ -5,18 +5,16 @@ class Solution:
         left, right = 0, 0
         trappedWater = 0
 
-        for i in range(len(height)):
-            if i - 1 >= 0:
-                maxLeft[i] = left
-            left = max(left, height[i])
+        maxLeft[0] = height[0]
+        for i in range(1, len(height)):
+            maxLeft[i] = max(maxLeft[i - 1], height[i])
         
-        for i in range(len(height) - 1, -1, -1):
-            if i + 1 < len(height):
-                maxRight[i] = (right)
-            right = max(right, height[i])   
+        maxRight[n - 1] = height[n - 1]
+        for i in range(len(height) - 2, -1, -1):
+            maxRight[i] = max(maxRight[i + 1], height[i])   
         
         for i in range(len(height)):
             containerSize = min(maxLeft[i], maxRight[i])
-            trappedWater += (max(containerSize - height[i], 0))
+            trappedWater += containerSize - height[i]
 
         return trappedWater
