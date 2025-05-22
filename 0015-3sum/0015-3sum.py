f870a8a8ae09faf08f91700630775d103170aaf3
Kushal Patel <61028285+kushalpatel2210@users.noreply.github.com>
2025-05-21 22:19:13 -0400

    Time: 459 ms (86.34%), Space: 20.7 MB (69.06%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -7,7 +7,7 @@ class Solution:
             if nums[i] > 0:
                 return res
 
-            if i > 0 and nums[i] == nums[i - 1]:
+            if i > 0 and nums[i - 1] == nums[i]:
                 continue
             
             l, r = i + 1, len(nums) - 1
