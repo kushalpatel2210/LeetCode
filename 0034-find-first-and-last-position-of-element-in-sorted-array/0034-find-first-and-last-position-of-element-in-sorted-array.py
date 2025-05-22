a528254970a342d14afa15040997a1e97f13e781
Kushal Patel <61028285+kushalpatel2210@users.noreply.github.com>
2025-05-21 21:37:55 -0400

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
@@ -1,39 +1,38 @@
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
-        l, r = 0, len(nums) - 1
-
-        def findFirst(l, r):
-            firstOcc = float('inf')
-            while l <= r:
-                mid = l + (r - l) // 2
-
-                if nums[mid] == target:
-                    firstOcc = min(firstOcc, mid)
-                    r = mid - 1
-                elif nums[mid] < target:
-                    l = mid + 1
-                else:
-                    r = mid - 1
-            
-            return firstOcc
         
-        def findLast(l, r):
-            lastOcc = float('-inf')
+        def leftMostTarget():
+            leftMost = float('inf')
+            l, r = 0, len(nums) - 1
+
             while l <= r:
-                mid = l + (r - l) // 2
-
-                if nums[mid] == target:
-                    lastOcc = max(lastOcc, mid)
-                    l = mid + 1
-                elif nums[mid] < target:
-                    l = mid + 1
+                m = l + (r - l) // 2
+                
+                if nums[m] == target:
+                    leftMost = min(leftMost, m)
+                    r = m - 1
+                if nums[m] < target:
+                    l = m + 1
                 else:
-                    r = mid - 1
-            return lastOcc
-        
-        findFirstOcc = findFirst(l, r)
-        findLastOcc = findLast(l, r)
-
-        return [-1 if findFirstOcc == float('inf') else findFirstOcc, -1 if findLastOcc == float('-inf') else findLastOcc]
-
+                    r = m - 1
             
+            return leftMost if leftMost != float('inf') else -1
+        
+        def rightMostTarget():
+            rightMost = float('-inf')
+            l, r = 0, len(nums) - 1
+
+            while l <= r:
+                m = l + (r - l) // 2
+
+                if nums[m] == target:
+                    rightMost = max(rightMost, m)
+                    l = m + 1
+                if nums[m] < target:
+                    l = m + 1
+                else:
+                    r = m - 1
+            
+            return rightMost if rightMost != float('-inf') else -1
+
+        return [leftMostTarget(), rightMostTarget()]
