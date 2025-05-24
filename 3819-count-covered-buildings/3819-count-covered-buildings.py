973dc87254253274a1fdc55d885a5ea48e1ff3e5
Kushal Patel <61028285+kushalpatel2210@users.noreply.github.com>
2025-05-24 17:45:38 -0400

    Time: 366 ms (96.56%), Space: 60.2 MB (47%) - LeetHub

---
+++ 3819-count-covered-buildings/3819-count-covered-buildings.py
@@ -0,0 +1,28 @@
+from collections import defaultdict
+
+class Solution:
+    def countCoveredBuildings(self, n: int, buildings: List[List[int]]) -> int:
+        x_to_y = defaultdict(list)
+        y_to_x = defaultdict(list)
+        count = 0
+
+        for x, y in buildings:
+            x_to_y[x].append(y)
+            y_to_x[y].append(x)
+
+        for x in x_to_y:
+            x_to_y[x].sort()
+        
+        for y in y_to_x:
+            y_to_x[y].sort()
+        
+
+        for x, y in buildings:
+            horizontalBuildings = y_to_x[y]
+            verticalBuildings = x_to_y[x]
+
+
+            if horizontalBuildings[0] < x < horizontalBuildings[-1] and verticalBuildings[0] < y < verticalBuildings[-1]:
+                count += 1
+        
+        return count
