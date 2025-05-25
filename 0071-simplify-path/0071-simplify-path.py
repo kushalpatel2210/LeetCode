f15c478c79eb04f8315bfcef39e6faa99b9eda53
Kushal Patel <61028285+kushalpatel2210@users.noreply.github.com>
2025-05-25 11:06:16 -0400

    Time: 0 ms (100%), Space: 18.1 MB (7.48%) - LeetHub

--- 0071-simplify-path/0071-simplify-path.py
+++ 0071-simplify-path/0071-simplify-path.py
@@ -1,45 +1,16 @@
 class Solution:
     def simplifyPath(self, path: str) -> str:
-        stack = []
         paths = path.split('/')
+        stack = []
 
-        for curr in paths:
-            if curr == '..' and stack:
-                stack.pop()
-            elif curr != '' and curr != '.' and curr != '..':
-                stack.append(curr)
+        for p in paths:
+            if p:
+                if p == '.':
+                    continue
+                if p == '..':
+                    if stack:
+                        stack.pop()
+                else:
+                    stack.append(p)
 
-        return '/' + '/'.join(stack)
-
-'''
-import re
-
-class Solution:
-    def simplifyPath(self, path: str) -> str:
-        stack = path.split('/')
-        outputStack = []
-        simplified = '/'
-        pattern = r"[a-zA-Z._]+"
-
-        while stack:
-            curr = stack.pop()
-
-            if curr == '' or curr == '.':
-                continue
-            elif curr == '..':
-                count = 1
-                while stack and count > 0:
-                    curr_ele = stack.pop()
-                    if curr_ele == '.':
-                        continue
-                    if curr_ele == '..':
-                        count += 1
-                    elif re.fullmatch(pattern, curr_ele):
-                        count -= 1
-            else:
-                outputStack.append(curr)
-                
-        simplified += '/'.join(reversed(outputStack))
-
-        return simplified
-'''
+        return "/" + "/".join(stack)
