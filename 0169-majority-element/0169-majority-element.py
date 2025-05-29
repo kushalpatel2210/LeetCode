30da4a4d3077e0899416ef3f5ce7b90b4a2cd501
Kushal Patel <61028285+kushalpatel2210@users.noreply.github.com>
2025-05-29 19:48:39 -0400

    Time: 5 ms (65.43%), Space: 19.5 MB (19.67%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,15 +1,12 @@
+# Boyer-Moore Voting Algorithm
+
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        count = 0
-        majorityElement = None
+        count = res = 0
 
         for num in nums:
-            if not count:
-                majorityElement = num
-                count += 1
-            elif num == majorityElement:
-                count += 1
-            else:
-                count -= 1
+            if count == 0:
+                res = num
+            count += (1 if num == res else -1)
         
-        return majorityElement
+        return res
