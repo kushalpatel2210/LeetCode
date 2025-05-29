e71f9599d9def81151dee3d197d7abaa570ec669
Kushal Patel <61028285+kushalpatel2210@users.noreply.github.com>
2025-05-29 07:40:43 -0400

    Time: 0 ms (100%), Space: 17.9 MB (30.07%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -5,12 +5,11 @@ class Solution:
         def dfs(n):
             if n <= 2:
                 return n
-            
+
             if n in memo:
                 return memo[n]
-
+                
             memo[n] = dfs(n - 1) + dfs(n - 2)
-
             return memo[n]
         
         return dfs(n)
