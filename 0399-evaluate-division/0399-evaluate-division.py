9f4cede1b119b71bdea38d8b3b287c1118a01de4
Kushal Patel <61028285+kushalpatel2210@users.noreply.github.com>
2025-06-02 08:30:25 -0400

    Time: 0 ms (100%), Space: 17.8 MB (93.61%) - LeetHub

--- 0399-evaluate-division/0399-evaluate-division.py
+++ 0399-evaluate-division/0399-evaluate-division.py
@@ -3,27 +3,29 @@ from collections import defaultdict, deque
 class Solution:
     def calcEquation(self, equations: List[List[str]], values: List[float], queries: List[List[str]]) -> List[float]:
         adj = defaultdict(list)
-        
-        for i in range(len(equations)):
-            adj[equations[i][0]].append((equations[i][1], values[i]))
-            adj[equations[i][1]].append((equations[i][0], 1 / values[i]))
+
+        for i, eq in enumerate(equations):
+            adj[eq[0]].append((eq[1], values[i]))
+            adj[eq[1]].append((eq[0], 1 / values[i]))
         
         def dfs(src, dst):
             if src not in adj or dst not in adj:
                 return -1
-            
+
             q = deque([(src, 1)])
             visited = set()
 
             while q:
                 source, weight = q.popleft()
+
                 if source == dst:
                     return weight
                 
                 for nei, wei in adj[source]:
                     if nei not in visited:
-                        q.append((nei, weight * wei))
+                        q.append([nei, wei * weight])
                         visited.add(nei)
-            return -1 
-        
-        return [dfs(q[0], q[1]) for q in queries]
+
+            return -1
+
+        return [dfs(s, d) for s, d in queries]
