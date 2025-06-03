17d16fa306040e92f62b06f3db417668b48cc21c
Kushal Patel <61028285+kushalpatel2210@users.noreply.github.com>
2025-06-03 07:15:30 -0400

    Time: 15 ms (60.69%), Space: 18.2 MB (29.08%) - LeetHub

--- 2856-count-complete-subarrays-in-an-array/2856-count-complete-subarrays-in-an-array.py
+++ 2856-count-complete-subarrays-in-an-array/2856-count-complete-subarrays-in-an-array.py
@@ -1,3 +1,28 @@
+from collections import Counter
+
+class Solution:
+    def countCompleteSubarrays(self, nums: List[int]) -> int:
+        uniqueCount = len(set(nums))
+        frq = Counter()
+        count, length = 0, len(nums)
+        l = 0
+
+        for r in range(len(nums)):
+            frq[nums[r]] += 1
+
+            while len(frq) == uniqueCount:
+                count += length - r
+                frq[nums[l]] -= 1
+
+                if frq[nums[l]] == 0:
+                    frq.pop(nums[l])
+
+                l += 1
+        
+        return count
+
+'''
+# Brute force
 class Solution:
     def countCompleteSubarrays(self, nums: List[int]) -> int:
         distinctElements = set(nums)
@@ -12,3 +37,4 @@ class Solution:
                     count += 1
 
         return count
+'''
