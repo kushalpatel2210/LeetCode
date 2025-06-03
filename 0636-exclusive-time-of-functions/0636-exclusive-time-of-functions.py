7463702e7c2112e66914e7ce6bc479d34278f56e
Kushal Patel <61028285+kushalpatel2210@users.noreply.github.com>
2025-06-03 19:06:06 -0400

    Time: 7 ms (69.76%), Space: 18.1 MB (24.08%) - LeetHub

--- 0636-exclusive-time-of-functions/0636-exclusive-time-of-functions.py
+++ 0636-exclusive-time-of-functions/0636-exclusive-time-of-functions.py
@@ -2,7 +2,7 @@ class Solution:
     def exclusiveTime(self, n: int, logs: List[str]) -> List[int]:
         exclusive_times = [0] * n
         call_stack = []
-        curr_time = -1
+        last_recorded_time = -1
 
         for log in logs:
             function_id, operation, timestamp = log.split(":")
@@ -11,12 +11,12 @@ class Solution:
 
             if operation == 'start':
                 if call_stack:
-                    exclusive_times[call_stack[-1]] += timestamp - curr_time
+                    exclusive_times[call_stack[-1]] += timestamp - last_recorded_time
                 call_stack.append(function_id)
-                curr_time = timestamp
+                last_recorded_time = timestamp
             else:
                 in_progress_function_id = call_stack.pop()
-                exclusive_times[in_progress_function_id] += timestamp - curr_time + 1
-                curr_time = timestamp + 1
+                exclusive_times[in_progress_function_id] += timestamp - last_recorded_time + 1
+                last_recorded_time = timestamp + 1
 
         return exclusive_times
