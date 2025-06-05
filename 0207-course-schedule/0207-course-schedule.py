83af5dfd823905d108b52ed90caee5b9aea9666b
Kushal Patel <61028285+kushalpatel2210@users.noreply.github.com>
2025-06-05 07:16:55 -0400

    Time: 6 ms (52.83%), Space: 19.1 MB (60.92%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -1,29 +1,31 @@
-# Topological sort 
 from collections import defaultdict, deque
 
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
         schedule = defaultdict(list)
-        inDegree = [0] * numCourses
+        inDegree = [0] * numCourses 
         q = deque()
+        coursesTaken = 0
 
         for crs, preq in prerequisites:
             schedule[crs].append(preq)
             inDegree[preq] += 1
-        
-        for i in range(len(inDegree)):
+            
+        for i in range(numCourses):
             if inDegree[i] == 0:
                 q.append(i)
         
-        coursesTaken = 0
         while q:
-            node = q.popleft()
-            coursesTaken += 1
+            n = len(q) # current length of the queue
+            
+            for _ in range(n):
+                crs = q.popleft()
+                coursesTaken += 1
 
-            for crs in schedule[node]:
-                inDegree[crs] -= 1
+                for course in schedule[crs]:
+                    inDegree[course] -= 1
 
-                if inDegree[crs] == 0:
-                    q.append(crs)
+                    if inDegree[course] == 0:
+                        q.append(course)
         
         return coursesTaken == numCourses
