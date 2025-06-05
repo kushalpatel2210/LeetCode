583932f563f57ed839bbdbcfac7c26f46c1fdc78
Kushal Patel <61028285+kushalpatel2210@users.noreply.github.com>
2025-06-05 08:45:29 -0400

    Time: 2 ms (82.08%), Space: 19.2 MB (47.83%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.py
+++ 0210-course-schedule-ii/0210-course-schedule-ii.py
@@ -1,32 +1,30 @@
-# Topological Sort
 from collections import defaultdict, deque
 
 class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
-        q = deque()
-        courses = defaultdict(list)
+        schedule = defaultdict(list)
         inDegree = [0] * numCourses
+        completedCourses = 0
+        q = deque()
         res = []
 
         for crs, preq in prerequisites:
-            courses[crs].append(preq)
+            schedule[crs].append(preq)
             inDegree[preq] += 1
         
-        for i in range(len(inDegree)):
+        for i in range(numCourses):
             if inDegree[i] == 0:
                 q.append(i)
-        
-        coursesTaken = 0
 
         while q:
-            currCrs = q.popleft()
-            res.append(currCrs)
-            coursesTaken += 1
+            crs = q.popleft()
+            completedCourses += 1
+            res.append(crs)
 
-            for course in courses[currCrs]:
+            for course in schedule[crs]:
                 inDegree[course] -= 1
-
+                
                 if inDegree[course] == 0:
                     q.append(course)
-
-        return res[::-1] if coursesTaken == numCourses else []
+        
+        return res[::-1] if completedCourses == numCourses else []
