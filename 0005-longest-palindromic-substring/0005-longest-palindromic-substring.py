c67b5cf39fc7be475ecc8e3210abb7a8552ab2b3
Kushal Patel <61028285+kushalpatel2210@users.noreply.github.com>
2025-06-06 18:37:33 -0400

    Time: 311 ms (57.52%), Space: 17.8 MB (60.94%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -1,25 +1,25 @@
 class Solution:
-    def expandPalindrome(self, left, right, s):
+    def expandString(self, left, right, s):
         while left > 0 and right < len(s) - 1 and s[left - 1] == s[right + 1]:
             left -= 1
             right += 1
         return (left, right - left + 1)
 
     def longestPalindrome(self, s: str) -> str:
-        start = maxLength = 0
+        start = maxLength = 0 
 
         for center in range(len(s)):
-            # odd length 'c'
-            oddStart, oddMaxLength = self.expandPalindrome(center, center, s)
-            if oddMaxLength > maxLength:
+            # odd start
+            oddStart, maxOddLength = self.expandString(center, center, s)
+            if maxOddLength > maxLength:
                 start = oddStart
-                maxLength = oddMaxLength
+                maxLength = maxOddLength
 
-            # even length like 'cc'
+            # even start 'cc'
             if center < len(s) - 1 and s[center] == s[center + 1]:
-                evenStart, evenMaxLength = self.expandPalindrome(center, center + 1, s)
-                if evenMaxLength > maxLength:
-                    maxLength = evenMaxLength
+                evenStart, maxEvenLength = self.expandString(center, center + 1, s)
+                if maxEvenLength > maxLength:
                     start = evenStart
-        
+                    maxLength = maxEvenLength
+
         return s[start: start + maxLength]
