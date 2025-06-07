b775217253bd45eeabc984b24d2141bdd2ea6334
Kushal Patel <61028285+kushalpatel2210@users.noreply.github.com>
2025-06-07 09:30:09 -0400

    Time: 6 ms (52.78%), Space: 18.9 MB (92.96%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -1,28 +1,28 @@
-from collections import defaultdict, deque
+from collections import deque, defaultdict
 
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        schedule = defaultdict(list)
-        inDegree = [0] * numCourses 
         q = deque()
+        schedule = defaultdict(list) # Preq, course
         coursesTaken = 0
+        inDegree = [0] * numCourses
 
         for crs, preq in prerequisites:
             schedule[preq].append(crs)
             inDegree[crs] += 1
-            
+        
         for i in range(numCourses):
             if inDegree[i] == 0:
                 q.append(i)
         
         while q:
-            crs = q.popleft()
+            currCrs = q.popleft()
             coursesTaken += 1
 
-            for course in schedule[crs]:
-                inDegree[course] -= 1
+            for crs in schedule[currCrs]:
+                inDegree[crs] -= 1
 
-                if inDegree[course] == 0:
-                    q.append(course)
+                if inDegree[crs] == 0:
+                    q.append(crs)
         
         return coursesTaken == numCourses
