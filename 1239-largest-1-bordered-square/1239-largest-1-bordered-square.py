11d98a4542abe55855639d66b0f89fcbcc63b29f
Kushal Patel <61028285+kushalpatel2210@users.noreply.github.com>
2025-06-07 11:35:21 -0400

    Time: 131 ms (61.19%), Space: 18.3 MB (21.98%) - LeetHub

--- 1239-largest-1-bordered-square/1239-largest-1-bordered-square.py
+++ 1239-largest-1-bordered-square/1239-largest-1-bordered-square.py
@@ -2,14 +2,14 @@ class Solution:
     def largest1BorderedSquare(self, grid: List[List[int]]) -> int:
         rows, cols = len(grid), len(grid[0])
 
-        down = [[0] * cols for _ in range(rows)]
-        right = [[0] * cols for _ in range(rows)]
+        down = [[0] * (cols + 1) for _ in range(rows + 1)]
+        right = [[0] * (cols + 1) for _ in range(rows + 1)]
 
         for row in range(rows - 1, -1, -1):
             for col in range(cols - 1, -1, -1):
                 if grid[row][col] == 1:
-                    down[row][col] = 1 + (down[row + 1][col] if row + 1 < rows else 0)
-                    right[row][col] = 1 + (right[row][col + 1] if col + 1 < cols else 0)
+                    down[row][col] = 1 + down[row + 1][col] 
+                    right[row][col] = 1 + right[row][col + 1]
         
         for max_side in range(min(rows, cols), 0, -1):
             for row in range(rows - max_side + 1):
