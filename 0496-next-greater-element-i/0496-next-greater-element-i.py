1293c07cda431441764c39c9d432c7e37c861297
Kushal Patel <61028285+kushalpatel2210@users.noreply.github.com>
2025-06-08 14:17:25 -0400

    Time: 0 ms (100%), Space: 18 MB (65.34%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.py
+++ 0496-next-greater-element-i/0496-next-greater-element-i.py
@@ -2,16 +2,17 @@ class Solution:
     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
         stack = [] # monotonically decreasing
         suffix = [-1] * len(nums2)
+        numIdx = dict()
         res = []
 
         for i, n in enumerate(nums2):
+            numIdx[n] = i
             while stack and stack[-1][0] <= n:
                 num, idx = stack.pop()
                 suffix[idx] = n
             stack.append((n, i))
 
         for n in nums1:
-            idxOfCurrNumInNums2 = nums2.index(n)
-            res.append(suffix[idxOfCurrNumInNums2])
+            res.append(suffix[numIdx[n]])
 
         return res
