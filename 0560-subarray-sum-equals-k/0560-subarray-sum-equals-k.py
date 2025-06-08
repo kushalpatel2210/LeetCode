fc1e4c1f59a24a6046924d9c2db7acdfaf6a9daa
Kushal Patel <61028285+kushalpatel2210@users.noreply.github.com>
2025-06-08 12:09:56 -0400

    Time: 35 ms (49.65%), Space: 20.4 MB (39.25%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
@@ -2,32 +2,17 @@ from collections import defaultdict
 
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
-        count = prefixSum = 0
-        prefixSums = defaultdict(int)
-        prefixSums[0] = 1
+        prefixSum = 0
+        hashMap = defaultdict(int) # prefix: count
+        hashMap[0] = 1
+        count = 0
 
         for num in nums:
             prefixSum += num
 
-            if prefixSum - k in prefixSums:
-                count += prefixSums[prefixSum - k]
+            if prefixSum - k in hashMap:
+                count += hashMap[prefixSum - k]
             
-            prefixSums[prefixSum] += 1
-
-        return count
-
-'''
-# Brute Force
-class Solution:
-    def subarraySum(self, nums: List[int], k: int) -> int:
-        subArrays = 0
-
-        for i in range(len(nums)):
-            currSum = 0
-            for j in range(i, len(nums)):
-                currSum += nums[j]
-                if currSum == k:
-                    subArrays += 1
+            hashMap[prefixSum] += 1
         
-        return subArrays
-'''
+        return count
