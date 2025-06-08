12e782020a432494d3deefcb3e213308cb8fd72b
Kushal Patel <61028285+kushalpatel2210@users.noreply.github.com>
2025-06-08 09:57:38 -0400

    Time: 11 ms (44.47%), Space: 18.6 MB (74.83%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.py
+++ 0045-jump-game-ii/0045-jump-game-ii.py
@@ -1,39 +1,13 @@
 class Solution:
     def jump(self, nums: List[int]) -> int:
-        res = 0
-        l = r = 0
+        l = r = res = 0
 
-        while r < len(nums) - 1:
+        while r < len(nums) - 1: # len(nums) - 1 is the target
             farthest = 0
-            for i in range(l, r + 1):
+            for i in range(l, r + 1): # l to r because the last element in the range is not inclusive
                 farthest = max(farthest, i + nums[i])
             l = r + 1
             r = farthest
             res += 1
         
         return res
-
-'''
-DP: Top Down 
-class Solution:
-    def jump(self, nums: List[int]) -> int:
-        memo = {}
-
-        def dfs(i):
-            if i in memo:
-                return memo[i]
-            if i == len(nums) - 1:
-                return 0
-            if nums[i] == 0:
-                return float('inf')
-            
-            minJump = float('inf')
-            end = min(len(nums), i + nums[i] + 1)
-            for j in range(i + 1, end):
-                minJump = min(minJump, 1 + dfs(j))
-                    
-            memo[i] = minJump
-            return minJump
-
-        return dfs(0)
-'''
