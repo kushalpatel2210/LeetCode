6da9cbd3735177fc2838c722e7a6d1bf50d66cb4
Kushal Patel <61028285+kushalpatel2210@users.noreply.github.com>
2025-06-09 08:10:15 -0400

    Time: 27 ms (67.63%), Space: 18.4 MB (90.31%) - LeetHub

--- 1049-minimum-domino-rotations-for-equal-row/1049-minimum-domino-rotations-for-equal-row.py
+++ 1049-minimum-domino-rotations-for-equal-row/1049-minimum-domino-rotations-for-equal-row.py
@@ -1,18 +1,19 @@
-from collections import defaultdict
-
 class Solution:
     def minDominoRotations(self, tops: List[int], bottoms: List[int]) -> int:
         def check(x):
             rotations_top = rotations_bottom = 0
+
             for i in range(len(tops)):
                 if tops[i] != x and bottoms[i] != x:
-                    return float('inf')  # Not possible
+                    return float('inf')
                 elif tops[i] != x:
                     rotations_top += 1
                 elif bottoms[i] != x:
                     rotations_bottom += 1
+            
             return min(rotations_top, rotations_bottom)
-
-        candidates = [tops[0], bottoms[0]]
-        min_rotations = min(check(candidates[0]), check(candidates[1]))
-        return -1 if min_rotations == float('inf') else min_rotations
+        
+        # It is guaranteed that if the solution exists then top or col row must be formed using first elment of either one of it.
+        minRotations = min(check(tops[0]), check(bottoms[0]))
+        return minRotations if minRotations != float('inf') else -1
+                
