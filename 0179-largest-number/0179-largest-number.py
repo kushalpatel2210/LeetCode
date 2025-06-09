ae9a90c13c2e9238ba9da39d6210a14088ffdfce
Kushal Patel <61028285+kushalpatel2210@users.noreply.github.com>
2025-06-09 18:04:37 -0400

    Time: 3 ms (53.95%), Space: 18 MB (16.25%) - LeetHub

--- 0179-largest-number/0179-largest-number.py
+++ 0179-largest-number/0179-largest-number.py
@@ -2,15 +2,15 @@ from functools import cmp_to_key
 
 class Solution:
     def largestNumber(self, nums: List[int]) -> str:
+        
         for i, num in enumerate(nums):
             nums[i] = str(num)
-        
-        def compare(n1, n2):
-            if n1 + n2 > n2 + n1:
+
+        def compare(x, y):
+            if x + y > y + x:
                 return -1
             else:
                 return 1
 
         nums = sorted(nums, key = cmp_to_key(compare))
-
         return str(int("".join(nums)))
