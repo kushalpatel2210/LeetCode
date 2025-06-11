6385b4ad1384831c211b066aa5d85159c048e0fe
Kushal Patel <61028285+kushalpatel2210@users.noreply.github.com>
2025-06-11 08:16:46 -0400

    Time: 1805 ms (64.71%), Space: 21.7 MB (14.54%) - LeetHub

--- 3751-maximum-frequency-after-subarray-operation/3751-maximum-frequency-after-subarray-operation.py
+++ 3751-maximum-frequency-after-subarray-operation/3751-maximum-frequency-after-subarray-operation.py
@@ -2,25 +2,25 @@ from collections import Counter
 
 class Solution:
     def maxOccurences(self, nums, target, k):
-        maxCount = currCount = 0
+        maxOcc = currOcc = 0
 
         for num in nums:
             if num == k:
-                currCount -= 1
-            
+                currOcc -= 1
+
             if num == target:
-                currCount += 1
+                currOcc += 1
             
-            if currCount < 0:
-                currCount = 0
-            
-            maxCount = max(maxCount, currCount)
+            if currOcc < 0:
+                currOcc = 0
+
+            maxOcc = max(maxOcc, currOcc)
         
-        return maxCount
+        return maxOcc
 
     def maxFrequency(self, nums: List[int], k: int) -> int:
         counter = Counter(nums)
-        maxFreq = 0
+        maxFreq = float(-inf)
 
         for key, frq in counter.items():
             maxFreq = max(maxFreq, self.maxOccurences(nums, key, k))
