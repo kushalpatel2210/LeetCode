ddccf2bacce02d72bac109ccbedf2a07b5fc49ee
Kushal Patel <61028285+kushalpatel2210@users.noreply.github.com>
2025-06-14 10:13:42 -0400

    Time: 3 ms (7%), Space: 18.8 MB (38.05%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.py
+++ 0206-reverse-linked-list/0206-reverse-linked-list.py
@@ -5,6 +5,18 @@
 #         self.next = next
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head or not head.next:
+            return head
+        
+        new_head = self.reverseList(head.next)
+        head.next.next = head
+        head.next = None
+        return new_head
+
+'''
+# Pointer
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
         prev, curr = None, head
 
         while curr:
@@ -14,3 +26,4 @@ class Solution:
             curr = nxt
         
         return prev
+'''
