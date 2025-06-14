14be2476c2597f31391c7ba3cb932ffe68746bda
Kushal Patel <61028285+kushalpatel2210@users.noreply.github.com>
2025-06-14 10:13:56 -0400

    Time: 0 ms (100%), Space: 18.8 MB (14.71%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.py
+++ 0206-reverse-linked-list/0206-reverse-linked-list.py
@@ -3,6 +3,8 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+
+# Recursion
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
         if not head or not head.next:
