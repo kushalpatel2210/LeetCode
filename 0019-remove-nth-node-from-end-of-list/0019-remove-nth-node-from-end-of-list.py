867ba257e5aec5e27e0a5ceda4181cf5ad4aec8f
Kushal Patel <61028285+kushalpatel2210@users.noreply.github.com>
2025-06-15 10:26:49 -0400

    Time: 1 ms (9.29%), Space: 17.7 MB (84.92%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.py
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.py
@@ -5,38 +5,17 @@
 #         self.next = next
 class Solution:
     def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
-        node = ListNode()
-        node.next = head
-        slow, fast = node, node
-
-        for _ in range(n + 1):
-            fast = fast.next
-
-        while fast:
-            slow = slow.next
-            fast = fast.next
-        
-        slow.next = slow.next.next
-
-        return node.next
-
-'''
-class Solution:
-    def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
-        node = head
-        res = dummy = ListNode()
+        dummy = ListNode()
         dummy.next = head
-        length = 0
+        trailer, leader = dummy, dummy
 
-        while node:
-            length += 1
-            node = node.next
+        for _ in range(n):
+            leader = leader.next
         
-        for i in range(length):
-            if i == length - n:
-                dummy.next = dummy.next.next
-                break
-            dummy = dummy.next
+        while leader.next:
+            leader = leader.next
+            trailer = trailer.next
         
-        return res.next
-'''
+        trailer.next = trailer.next.next
+
+        return dummy.next
