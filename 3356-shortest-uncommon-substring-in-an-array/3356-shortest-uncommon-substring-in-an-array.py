7b408e21e871263cae68367e4e8bd58a66ea4237
Kushal Patel <61028285+kushalpatel2210@users.noreply.github.com>
2025-09-24 08:09:39 -0400

    Time: 7375 ms (5.04%), Space: 18.8 MB (53.56%) - LeetHub

---
+++ 3356-shortest-uncommon-substring-in-an-array/3356-shortest-uncommon-substring-in-an-array.py
@@ -0,0 +1,30 @@
+from collections import defaultdict
+
+class Solution:
+    def shortestSubstrings(self, arr: List[str]) -> List[str]:
+        s = arr[0]
+        subStringsMap = defaultdict(set)
+        shortest = []
+
+        for idx, word in enumerate(arr):
+            for i in range(len(word)):
+                for j in range(i + 1, len(word) + 1):
+                    subStringsMap[idx].add(word[i:j])
+        
+        for key in subStringsMap:
+            values = sorted(list(subStringsMap[key]), key=lambda x: (len(x), x))    
+            combinedSet = set()
+
+            for i in range(len(subStringsMap)):
+                if i != key:
+                    combinedSet = combinedSet.union(subStringsMap[i])
+            
+            res = ''
+            for subString in values:
+                if subString not in combinedSet:
+                    res = subString
+                    break
+
+            shortest.append(res)
+
+        return shortest
