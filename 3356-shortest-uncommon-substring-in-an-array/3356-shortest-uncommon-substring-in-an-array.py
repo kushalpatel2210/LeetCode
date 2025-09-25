daef12952e532e243c056f834d9495127dc38c4e
Kushal Patel <61028285+kushalpatel2210@users.noreply.github.com>
2025-09-25 08:07:54 -0400

    Time: 218 ms (39.77%), Space: 21 MB (8.63%) - LeetHub

--- 3356-shortest-uncommon-substring-in-an-array/3356-shortest-uncommon-substring-in-an-array.py
+++ 3356-shortest-uncommon-substring-in-an-array/3356-shortest-uncommon-substring-in-an-array.py
@@ -1,15 +1,15 @@
 class TrieNode:
     def __init__(self):
-        self.children = {}
-        # set of word indices that contain this substring
-        self.word_ids = set()
+        self.children = dict()
+        self.words_id = set()
 
 class Trie:
     def __init__(self):
         self.root = TrieNode()
-
+    
     def insert(self, word, word_id):
         n = len(word)
+
         for i in range(n):
             curr = self.root
             for j in range(i, n):
@@ -17,27 +17,30 @@ class Trie:
                 if ch not in curr.children:
                     curr.children[ch] = TrieNode()
                 curr = curr.children[ch]
-                curr.word_ids.add(word_id)
+                curr.words_id.add(word_id)
     
     def find(self, word, word_id):
+        n = len(word)
+
         best = ""
-        for i in range(len(word)):
+        for i in range(n):
             curr = self.root
             substring = ""
-            for j in range(i, len(word)):
+            for j in range(i, n):
                 ch = word[j]
+
                 if ch not in curr.children:
                     break
+                
                 curr = curr.children[ch]
                 substring += ch
 
-                if curr.word_ids == {word_id}:
+                if curr.words_id == {word_id}: # unique
                     if (best == "" or len(substring) < len(best) or (len(substring) == len(best) and substring < best)):
                         best = substring
-                    break
+                    break # once we find the word no need to go in-depth as length of word will always be higher
         
         return best
-        
 
 class Solution:
     def shortestSubstrings(self, arr: List[str]) -> List[str]:
@@ -45,7 +48,7 @@ class Solution:
 
         for idx, word in enumerate(arr):
             trie.insert(word, idx)
-        
+
         result = []
         for idx, word in enumerate(arr):
             result.append(trie.find(word, idx))
