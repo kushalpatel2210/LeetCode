ba2a088fd8cebdcd6317ab5feb3e7f2084ebc254
Kushal Patel <61028285+kushalpatel2210@users.noreply.github.com>
2025-09-25 06:51:19 -0400

    Time: 175 ms (64.03%), Space: 19.8 MB (40.64%) - LeetHub

--- 3356-shortest-uncommon-substring-in-an-array/3356-shortest-uncommon-substring-in-an-array.py
+++ 3356-shortest-uncommon-substring-in-an-array/3356-shortest-uncommon-substring-in-an-array.py
@@ -1,26 +1,48 @@
-from collections import defaultdict
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.count = 0  # number of distinct words containing this substring
+
 
 class Solution:
-    def shortestSubstrings(self, arr: List[str]) -> List[str]:
-        s = arr[0]
-        subStringsMap = defaultdict(set)
-        shortest = []
+    def buildTrie(self, arr):
+        root = TrieNode()
+        for word in arr:
+            visited_nodes = set()  # track nodes visited for this word
+            n = len(word)
+            for i in range(n):
+                node = root
+                for j in range(i, n):
+                    ch = word[j]
+                    if ch not in node.children:
+                        node.children[ch] = TrieNode()
+                    node = node.children[ch]
 
-        for idx, word in enumerate(arr):
-            for i in range(len(word)):
-                for j in range(i + 1, len(word) + 1):
-                    subStringsMap[idx].add(word[i:j])
-        
-        for key in subStringsMap:
-            values = sorted(list(subStringsMap[key]), key=lambda x: (len(x), x))    
-            combinedSet = set().union(*(subStringsMap[i] for i in range(len(subStringsMap)) if i != key))
-            
-            res = ''
-            for subString in values:
-                if subString not in combinedSet:
-                    res = subString
+                    # increment count only once per word per node
+                    if node not in visited_nodes:
+                        node.count += 1
+                        visited_nodes.add(node)
+        return root
+
+    def findUniqueSubstring(self, word, root):
+        n = len(word)
+        best = None
+        for i in range(n):
+            node = root
+            substring = ""
+            for j in range(i, n):
+                ch = word[j]
+                if ch not in node.children:
                     break
+                node = node.children[ch]
+                substring += ch
+                if node.count == 1:  # unique substring found
+                    if best is None or len(substring) < len(best) or \
+                       (len(substring) == len(best) and substring < best):
+                        best = substring
+                    break
+        return best if best else ""
 
-            shortest.append(res)
-
-        return shortest
+    def shortestSubstrings(self, arr):
+        root = self.buildTrie(arr)
+        return [self.findUniqueSubstring(word, root) for word in arr]
