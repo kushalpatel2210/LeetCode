9bb70b2d0cbe89d3762c59a2092dab8742df2f9b
Kushal Patel <61028285+kushalpatel2210@users.noreply.github.com>
2025-10-01 07:19:33 -0400

    Time: 208 ms (45.09%), Space: 21 MB (9.55%) - LeetHub

--- 3356-shortest-uncommon-substring-in-an-array/3356-shortest-uncommon-substring-in-an-array.py
+++ 3356-shortest-uncommon-substring-in-an-array/3356-shortest-uncommon-substring-in-an-array.py
@@ -1,7 +1,7 @@
 class TrieNode:
     def __init__(self):
         self.children = dict()
-        self.words_id = set()
+        self.word_ids = set()
 
 class Trie:
     def __init__(self):
@@ -17,12 +17,12 @@ class Trie:
                 if ch not in curr.children:
                     curr.children[ch] = TrieNode()
                 curr = curr.children[ch]
-                curr.words_id.add(word_id)
+                curr.word_ids.add(word_id)
     
     def find(self, word, word_id):
         n = len(word)
-
         best = ""
+
         for i in range(n):
             curr = self.root
             substring = ""
@@ -35,22 +35,23 @@ class Trie:
                 curr = curr.children[ch]
                 substring += ch
 
-                if curr.words_id == {word_id}: # unique
+                if curr.word_ids == {word_id}: # unique
                     if (best == "" or len(substring) < len(best) or (len(substring) == len(best) and substring < best)):
                         best = substring
-                    break # once we find the word no need to go in-depth as length of word will always be higher
-        
+                    break
+
         return best
 
 class Solution:
     def shortestSubstrings(self, arr: List[str]) -> List[str]:
         trie = Trie()
 
-        for idx, word in enumerate(arr):
-            trie.insert(word, idx)
-
-        result = []
-        for idx, word in enumerate(arr):
-            result.append(trie.find(word, idx))
+        for i, word in enumerate(arr):
+            trie.insert(word, i)
         
-        return result
+        res = []
+        for i, word in enumerate(arr):
+            shortest = trie.find(word, i)
+            res.append(shortest)
+        
+        return res
