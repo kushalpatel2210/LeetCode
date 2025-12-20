3ee4eef96f105a08505b633d9568daaad38c99e6
Kushal Patel <61028285+kushalpatel2210@users.noreply.github.com>
2025-12-20 07:39:18 -0500

    Time: 7 ms (51.35%), Space: 22.5 MB (26.59%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,3 +1,46 @@
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        """ Bucket Sort """
+        count = dict()
+        frq = [[] for i in range(len(nums) + 1)]
+        res = []
+        
+        for num in nums:
+            count[num] = 1 + count.get(num, 0)
+    
+        for num, cnt in count.items():
+            frq[cnt].append(num)
+        
+        for i in range(len(frq) - 1, 0, -1):
+            for num in frq[i]:
+                res.append(num)
+
+                if len(res) == k:
+                    return res
+
+"""
+Sol 1: Sorting 
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        count = dict()
+
+        for num in nums:
+            count[num] = 1 + count.get(num, 0)
+        
+        arr = []
+        for num, cnt in count.items():
+            arr.append([cnt, num])
+        arr.sort()
+
+        res = []
+        while len(res) < k:
+            res.append(arr.pop()[1])
+        
+        return res
+"""
+
+"""
+Sol 2: MinHeap
 import heapq
 
 class Solution:
@@ -17,27 +60,6 @@ class Solution:
         res = []
         for i in range(k):
             res.append(heapq.heappop(minHeap)[1])
-        
-        return res
-
-
-"""
-Sol 1: Sorting 
-class Solution:
-    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        count = dict()
-
-        for num in nums:
-            count[num] = 1 + count.get(num, 0)
-        
-        arr = []
-        for num, cnt in count.items():
-            arr.append([cnt, num])
-        arr.sort()
-
-        res = []
-        while len(res) < k:
-            res.append(arr.pop()[1])
         
         return res
 """
