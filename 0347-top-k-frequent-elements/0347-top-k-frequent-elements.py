5c4a46bbb26935a063d592b20c9191654b3353ed
Kushal Patel <61028285+kushalpatel2210@users.noreply.github.com>
2025-12-20 07:27:43 -0500

    Time: 9 ms (28.31%), Space: 20.6 MB (99.92%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,3 +1,28 @@
+import heapq
+
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        count = dict()
+
+        for num in nums:
+            count[num] = 1 + count.get(num, 0)
+        
+        minHeap = []
+        for num, cnt in count.items():
+            heapq.heappush(minHeap, (cnt, num))
+
+            if len(minHeap) > k:
+                heapq.heappop(minHeap)
+        
+        res = []
+        for i in range(k):
+            res.append(heapq.heappop(minHeap)[1])
+        
+        return res
+
+
+"""
+Sol 1: Sorting 
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         count = dict()
@@ -15,3 +40,4 @@ class Solution:
             res.append(arr.pop()[1])
         
         return res
+"""
