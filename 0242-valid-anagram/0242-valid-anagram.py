da2c03bc435e875cd662dd891ec3ecfda694365b
Kushal Patel <61028285+kushalpatel2210@users.noreply.github.com>
2025-12-20 06:56:04 -0500

    Time: 11 ms (73.34%), Space: 17.3 MB (100%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,6 +1,23 @@
+from collections import defaultdict
+
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        
+        count_s, count_t = defaultdict(int), defaultdict(int)
+        for i in range(len(s)):
+            count_s[s[i]] += 1
+            count_t[t[i]] += 1
+        
+        return count_s == count_t
+
+"""
+Sol 1:
 from collections import Counter
 
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         count_s, count_t = Counter(s), Counter(t)
         return count_s == count_t
+"""
