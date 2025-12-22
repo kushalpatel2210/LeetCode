85ff8536a7f33113246a00c5ec7e4d836d4c0e47
Kushal Patel <61028285+kushalpatel2210@users.noreply.github.com>
2025-12-22 11:26:29 -0500

    Time: 565 ms (63.64%), Space: 20.1 MB (98.03%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,28 +1,31 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        nums.sort()
         res = []
+        nums.sort()
 
-        for i in range(len(nums)):
+        for i, num in enumerate(nums):
+            if num > 0:
+                break
+
             if i > 0 and nums[i] == nums[i - 1]:
                 continue
-            
-            l, r = i + 1, len(nums) - 1
 
-            while l < r:
-                currSum = nums[i] + nums[l] + nums[r]
+            j, k = i + 1, len(nums) - 1
 
-                if currSum == 0:
-                    res.append([nums[i], nums[l], nums[r]])
-                    l += 1
-                    r -= 1
-
-                    while l < r and nums[l] == nums[l - 1]:
-                        l += 1
-                elif currSum > 0:
-                    r -= 1
+            while j < len(nums) and j < k:
+                target = 0 - num
+                curr_sum = nums[j] + nums[k]
+                if curr_sum > target:
+                    k -= 1
+                elif curr_sum < target:
+                    j += 1
                 else:
-                    l += 1
+                    res.append([num, nums[j], nums[k]])
+                    j += 1
+                    k -= 1
+                    while j < k and nums[j] == nums[j - 1]:
+                        j += 1
+                    while j < k and nums[k] == nums[k + 1]:
+                        k -= 1
         
         return res
-
