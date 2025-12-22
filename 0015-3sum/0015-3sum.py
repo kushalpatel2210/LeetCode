1f340b9688d10a091894ec6cfb1db40a188daa57
Kushal Patel <61028285+kushalpatel2210@users.noreply.github.com>
2025-12-22 11:27:55 -0500

    Time: 570 ms (61.75%), Space: 20.1 MB (98.03%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -25,7 +25,5 @@ class Solution:
                     k -= 1
                     while j < k and nums[j] == nums[j - 1]:
                         j += 1
-                    while j < k and nums[k] == nums[k + 1]:
-                        k -= 1
         
         return res
