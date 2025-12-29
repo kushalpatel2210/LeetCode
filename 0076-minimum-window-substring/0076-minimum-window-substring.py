9feed786ddd222ed0a2cec8319ac6fb61f108556
Kushal Patel <61028285+kushalpatel2210@users.noreply.github.com>
2025-12-29 06:29:59 -0500

    Time: 99 ms (40.85%), Space: 17.7 MB (99.56%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -2,27 +2,30 @@ from collections import Counter
 
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        if len(t) > len(s):
-            return ""
-        
         l = 0
-        ans = ""
-        len_ans = len(s)
-        counter_t = Counter(t)
-        counter = Counter()
+        min_window = float('inf')
+        count_t, count_s = Counter(t), Counter() # empty
+        have, need = 0, len(count_t)
+        res = [-1, -1] # pointers
 
         for r in range(len(s)):
-            if s[r] in t:
-                counter[s[r]] += 1
+            curr = s[r]
+            count_s[curr] += 1
 
-            while l <= r and all(counter[ch] >= counter_t[ch] for ch in counter_t):
-                if r - l + 1 <= len_ans:
-                    len_ans = r - l + 1
-                    ans = s[l : r + 1]
+            if curr in count_t and count_s[curr] == count_t[curr]:
+                have += 1
+            
+            while have == need:
+                if r - l + 1 < min_window:
+                    res = [l, r]
+                    min_window = r - l + 1
                 
-                if s[l] in counter:
-                    counter[s[l]] -= 1
+                count_s[s[l]] -= 1
 
+                if s[l] in count_t and count_s[s[l]] < count_t[s[l]]:
+                    have -= 1
+                
                 l += 1
         
-        return ans
+        l, r = res
+        return s[l: r + 1] if min_window != float('inf') else ""
