766e2880d368d9ba8b5e3c381aba7777fcd0396d
Kushal Patel <61028285+kushalpatel2210@users.noreply.github.com>
2025-12-30 12:09:54 -0500

    Time: 0 ms (100%), Space: 21.9 MB (94.98%) - LeetHub

--- 0143-reorder-list/0143-reorder-list.py
+++ 0143-reorder-list/0143-reorder-list.py
@@ -3,60 +3,33 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-
-# time O(n) space O(1)
 class Solution:
     def reorderList(self, head: Optional[ListNode]) -> None:
         """
         Do not return anything, modify head in-place instead.
         """
-        # find middle
-        slow, fast = head, head.next
+        # Find mid
+        slow = fast = head
         while fast and fast.next:
             slow = slow.next
             fast = fast.next.next
         
-        second = slow.next
+        # Reverse from mid
+        mid = slow.next
         slow.next = None
-
-        # reverse the second half
         prev = None
-        while second:
-            tmp = second.next
-            second.next = prev
-            prev = second 
-            second = tmp
+        while mid:
+            nxt = mid.next
+            mid.next = prev
+            prev = mid
+            mid = nxt
         
-        # merge two halfs 
+        # Re-arrange
         first, second = head, prev
         while second:
             tmp1, tmp2 = first.next, second.next
-            first.next = second
+            first.next = second 
             second.next = tmp1
-            first, second = tmp1, tmp2 
+            first, second = tmp1, tmp2
 
-'''
-# time and space O(n)
-class Solution:
-    def reorderList(self, head: Optional[ListNode]) -> None:
-        """
-        Do not return anything, modify head in-place instead.
-        """
-        stack = []
-        length = 0
-        dummy = head
-        
-        while dummy:
-            length += 1
-            stack.append(dummy)
-            dummy = dummy.next
 
-        curr = head
-        for _ in range((length - 1) // 2):
-            pop = stack.pop()
-            pop.next = curr.next
-            curr.next = pop
-            curr = pop.next
-        
-        stack.pop().next = None
-'''
