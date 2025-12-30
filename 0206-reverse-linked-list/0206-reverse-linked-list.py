8598baa3cdf638693311266b0f7ff355b1f92924
Kushal Patel <61028285+kushalpatel2210@users.noreply.github.com>
2025-12-30 07:20:03 -0500

    Time: 0 ms (100%), Space: 18.2 MB (86.59%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.py
+++ 0206-reverse-linked-list/0206-reverse-linked-list.py
@@ -3,29 +3,14 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-
-# Recursion
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        if not head or not head.next:
-            return head
-        
-        new_head = self.reverseList(head.next)
-        head.next.next = head
-        head.next = None
-        return new_head
+        prev = None
 
-'''
-# Pointer
-class Solution:
-    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        prev, curr = None, head
-
-        while curr:
-            nxt = curr.next
-            curr.next = prev
-            prev = curr
-            curr = nxt
+        while head:
+            nxt = head.next
+            head.next = prev
+            prev = head
+            head = nxt
         
         return prev
-'''
